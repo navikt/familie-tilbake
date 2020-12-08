ba58c1254095a01e41688c07a74192dceee855b8
Henning Solberg <henning.solberg@nav.no>
2020-12-08 22:17:58 +0100

    La inn en kjørende app som kan deployes

---
+++ src/main/kotlin/no/nav/familie/tilbake/FamilieTilbakeApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.familie.tilbake
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FamilieTilbakeApplication
+
+fun main(args: Array<String>) {
+	runApplication<FamilieTilbakeApplication>(*args)
+}
---
+++ src/test/kotlin/no/nav/familie/tilbake/FamilieTilbakeApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.familie.tilbake
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FamilieTilbakeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
