d87ac3268fa4ce4e3775c3958dbd2b7360574d71
Saikat Halder <saikat.halder@nav.no>
2021-01-05 17:31:40 +0100

    konfigurerte swagger for familie-tilbake

---
+++ src/main/kotlin/no/nav/familie/tilbake/config/SwaggerConfig.kt
@@ -0,0 +1,52 @@
+package no.nav.familie.tilbake.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.*
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+class SwaggerConfig {
+
+    private val bearer = "JWT"
+
+    @Bean
+    fun oppdragApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .securitySchemes(securitySchemes())
+            .securityContexts(securityContext())
+            .apiInfo(apiInfo()).select()
+            .apis(RequestHandlerSelectors.basePackage("no.nav.familie.tilbake"))
+            .paths(PathSelectors.any())
+            .build()
+    }
+
+    private fun securitySchemes(): List<ApiKey> {
+        return listOf(ApiKey(bearer, "Authorization", "header"))
+    }
+
+    private fun securityContext(): List<SecurityContext> {
+        return listOf(SecurityContext.builder()
+                          .securityReferences(defaultAuth())
+                          .forPaths(PathSelectors.regex("/api.*"))
+                          .build())
+    }
+
+    private fun defaultAuth(): List<SecurityReference> {
+        val authorizationScope = AuthorizationScope("global", "accessEverything")
+        val authorizationScopes = arrayOfNulls<AuthorizationScope>(1)
+        authorizationScopes[0] = authorizationScope
+        return listOf(SecurityReference("JWT", authorizationScopes))
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder().build()
+    }
+
+}
