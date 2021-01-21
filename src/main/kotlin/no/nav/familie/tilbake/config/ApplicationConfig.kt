2f134299b10e68f1805112df5f933610cafe0dc5
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2021-01-21 12:12:10 +0100

    Oppgradert spring (#11)
    * Oppgradert spring

--- src/main/kotlin/no/nav/familie/tilbake/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/tilbake/config/ApplicationConfig.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 import no.nav.familie.log.filter.LogFilter
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.SpringBootConfiguration
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan
 @SpringBootConfiguration
 @ComponentScan(ApplicationConfig.pakkenavn, "no.nav.familie.sikkerhet")
 @EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation.swagger"])
+@ConfigurationPropertiesScan
 class ApplicationConfig {
 
 
