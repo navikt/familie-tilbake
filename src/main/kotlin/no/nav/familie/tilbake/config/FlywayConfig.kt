8b26842f74ac81a390cfdb68a7128ef581c5e767
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-01-21 11:34:22 +0100

    Logger db-oppdatering

--- src/main/kotlin/no/nav/familie/tilbake/config/FlywayConfig.kt
+++ src/main/kotlin/no/nav/familie/tilbake/config/FlywayConfig.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.tilbake.config
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer
 import org.springframework.boot.context.properties.ConfigurationProperties
@@ -11,8 +12,11 @@ import org.springframework.context.annotation.Bean
 @ConstructorBinding
 data class FlywayConfig(private val role: String) {
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     @Bean
     fun flywayConfig(): FlywayConfigurationCustomizer {
+        logger.info("DB-oppdateringer kjøres med rolle $role")
         return FlywayConfigurationCustomizer {
             it.initSql(String.format("SET ROLE \"%s\"", role))
         }
