d438f4cc5fcadb9fe35e33d5474f6b25d52af84c
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-03-01 11:24:29 +0100

    Fjernet ubrukt kode

--- src/main/kotlin/no/nav/familie/tilbake/service/dokumentbestilling/brevmaler/Dokumentmalstype.kt
+++ src/main/kotlin/no/nav/familie/tilbake/service/dokumentbestilling/brevmaler/Dokumentmalstype.kt
@@ -1,9 +1,9 @@
 package no.nav.familie.tilbake.service.dokumentbestilling.brevmaler
 
 
-enum class Dokumentmalstype(val kode: String, val navn: String) {
-    INNHENT_DOKUMENTASJON("INNHEN", "Innhent dokumentasjon"),
-    FRITEKSTBREV("FRITKS", "Fritekstbrev"),
-    VARSEL("VARS", "Varsel om tilbakekreving"),
-    KORRIGERT_VARSEL("KORRIGVARS", "Korrigert varsel om tilbakebetaling");
+enum class Dokumentmalstype {
+    INNHENT_DOKUMENTASJON,
+    FRITEKSTBREV,
+    VARSEL,
+    KORRIGERT_VARSEL;
 }
--- src/main/kotlin/no/nav/familie/tilbake/service/dokumentbestilling/dto/BrevmalDto.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.familie.tilbake.service.dokumentbestilling.dto
-
-class BrevmalDto(var kode: String,
-                 val navn: String,
-                 val tilgjengelig: Boolean)
