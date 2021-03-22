a792930e0c95e06d55803f0de96049f6b4ad8a2c
Joakim Schweizer <joakim.schweizer@nav.no>
2021-03-22 12:52:41 +0100

    Fikser review comment

--- src/main/kotlin/no/nav/familie/tilbake/e2e/AutotestController.kt
+++ src/main/kotlin/no/nav/familie/tilbake/e2e/AutotestController.kt
@@ -1,12 +1,12 @@
 package no.nav.familie.tilbake.e2e
 
+import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.domene.TaskRepository
 import no.nav.familie.tilbake.kravgrunnlag.task.BehandleKravgrunnlagTask
 import no.nav.familie.tilbake.kravgrunnlag.task.BehandleStatusmeldingTask
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.context.annotation.Profile
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,21 +21,21 @@ import java.util.UUID
 class AutotestController (private val taskRepository: TaskRepository) {
 
     @PostMapping(path = ["/opprett/kravgrunnlag/"])
-    fun opprettKravgrunnlag(@RequestBody kravgrunnlag: String): HttpStatus {
+    fun opprettKravgrunnlag(@RequestBody kravgrunnlag: String): Ressurs<String> {
         taskRepository.save(Task(type = BehandleKravgrunnlagTask.TYPE,
                                  payload = kravgrunnlag,
                                  properties = Properties().apply {
                                      this["callId"] = UUID.randomUUID()
                                  }))
-        return HttpStatus.ACCEPTED
+        return Ressurs.success("")
     }
     @PostMapping(path = ["/opprett/statusmelding/"])
-    fun opprettStatusmelding(@RequestBody statusmelding: String): HttpStatus {
+    fun opprettStatusmelding(@RequestBody statusmelding: String): Ressurs<String> {
         taskRepository.save(Task(type = BehandleStatusmeldingTask.TYPE,
                                  payload = statusmelding,
                                  properties = Properties().apply {
                                      this["callId"] = UUID.randomUUID()
                                  }))
-        return HttpStatus.ACCEPTED
+        return Ressurs.success("")
     }
 }
