cec2ce1bee207a405a2f1801b6b6bd1b3fd28223
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-04-07 13:58:21 +0200

    Fikset bug i overlapper

---
+++ src/test/kotlin/no/nav/familie/tilbake/common/PeriodeTest.kt
@@ -0,0 +1,39 @@
+package no.nav.familie.tilbake.common
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.YearMonth
+
+internal class PeriodeTest {
+
+    @Test
+    fun `snitt returnerer lik periode for like perioder`() {
+        val periode1 = Periode(YearMonth.of(2019, 1), YearMonth.of(2019, 5))
+        val periode2 = Periode(YearMonth.of(2019, 1), YearMonth.of(2019, 5))
+        val snitt = periode1.snitt(periode2)
+
+        assertThat(snitt).isEqualTo(periode1)
+    }
+
+    @Test
+    fun `snitt returnerer null for periode uten overlap`() {
+        val periode1 = Periode(YearMonth.of(2019, 1), YearMonth.of(2019, 5))
+        val periode2 = Periode(YearMonth.of(2018, 1), YearMonth.of(2018, 12))
+        val snitt = periode1.snitt(periode2)
+
+        assertThat(snitt).isNull()
+    }
+
+    @Test
+    fun `snitt returnerer lik periode uansett hvilken periode som ligger til grunn`() {
+        val periode1 = Periode(YearMonth.of(2019, 1), YearMonth.of(2019, 5))
+        val periode2 = Periode(YearMonth.of(2019, 3), YearMonth.of(2019, 12))
+        val snitt1til2 = periode1.snitt(periode2)
+        val snitt2til1 = periode2.snitt(periode1)
+
+        assertThat(snitt1til2).isEqualTo(snitt2til1)
+        assertThat(snitt1til2).isEqualTo(Periode(YearMonth.of(2019, 3), YearMonth.of(2019, 5)))
+    }
+
+
+}
