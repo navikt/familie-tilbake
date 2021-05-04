ce511a190cabd293b7cccf7ac2da5a3945e1ecd7
Saikat Halder <45660476+haldersaikat@users.noreply.github.com>
2021-05-04 12:00:29 +0200

    Feature/status endring (#189)
    * endret behandlingsstatus basert på Behandlingsstegstilstand, fjernet fagsaksstaus
    
    * oppdaterte database skript versjon
    
    * endret default antall foreldele måned til 30 mnd
    
    * endret familie-ba-sak namespace til teamfamilie i prod
    
    * endret formattering

--- src/test/kotlin/no/nav/familie/tilbake/behandling/FagsakRepositoryTest.kt
+++ src/test/kotlin/no/nav/familie/tilbake/behandling/FagsakRepositoryTest.kt
@@ -1,7 +1,6 @@
 package no.nav.familie.tilbake.behandling
 
 import no.nav.familie.tilbake.OppslagSpringRunnerTest
-import no.nav.familie.tilbake.behandling.domain.Fagsaksstatus
 import no.nav.familie.tilbake.common.repository.findByIdOrThrow
 import no.nav.familie.tilbake.data.Testdata
 import org.assertj.core.api.Assertions.assertThat
@@ -29,7 +28,7 @@ internal class FagsakRepositoryTest : OppslagSpringRunnerTest() {
     fun `update med gyldige verdier skal oppdatere en forekomst av Fagsak i basen`() {
         fagsakRepository.insert(fagsak)
         var lagretFagsak = fagsakRepository.findByIdOrThrow(fagsak.id)
-        val oppdatertFagsak = lagretFagsak.copy(status = Fagsaksstatus.UNDER_BEHANDLING)
+        val oppdatertFagsak = lagretFagsak.copy(eksternFagsakId = "1")
 
         fagsakRepository.update(oppdatertFagsak)
 
