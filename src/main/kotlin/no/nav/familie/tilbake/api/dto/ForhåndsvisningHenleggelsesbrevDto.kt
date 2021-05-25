5c1b628d28245c6a3efef023893938e80cb245ef
Geir Roger Moen <58559732+grmoen-nav@users.noreply.github.com>
2021-05-25 13:04:11 +0200

    TEA-4537 Endring til endepunktet for å forhåndsvise henleggelsesbrev (#217)

---
+++ src/main/kotlin/no/nav/familie/tilbake/api/dto/ForhåndsvisningHenleggelsesbrevDto.kt
@@ -0,0 +1,8 @@
+package no.nav.familie.tilbake.api.dto
+
+import java.util.UUID
+import javax.validation.constraints.Size
+
+data class ForhåndsvisningHenleggelsesbrevDto(val behandlingId: UUID,
+                                              @Size(max = 1500, message = "Fritekst er for lang")
+                                              val fritekst: String?)
