9709da65339638a8f070bf700dbe231f037c1cc9
Saikat Halder <saikat.halder@nav.no>
2021-05-27 13:09:21 +0200

    kastet exception hvis historikkinnslag ikke kan sendes til kafka slik at tasken kan rekjøres.

--- src/main/kotlin/no/nav/familie/tilbake/integration/kafka/KafkaProducer.kt
+++ src/main/kotlin/no/nav/familie/tilbake/integration/kafka/KafkaProducer.kt
@@ -2,6 +2,7 @@ package no.nav.familie.tilbake.integration.kafka
 
 import no.nav.familie.kontrakter.felles.historikkinnslag.OpprettHistorikkinnslagRequest
 import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.tilbake.common.exceptionhandler.Feil
 import no.nav.familie.tilbake.config.Constants
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.slf4j.LoggerFactory
@@ -24,6 +25,8 @@ class KafkaProducer(private val kafkaTemplate: KafkaTemplate<String, String>) {
                              },
                              {
                                  log.warn("Historikkinnslag kan ikke sendes for $behandlingId med $key. Feiler med ${it.message}")
+                                 throw Feil(message = "Historikkinnslag kan ikke sendes for $behandlingId med $key. " +
+                                                      "Feiler med ${it.message}", it)
                              })
     }
 }
