27abdb467aba7b757e0fe0c74bcf86505d888516
Saikat Halder <45660476+haldersaikat@users.noreply.github.com>
2021-05-28 11:05:55 +0200

    la til KAFKA_BROKERS oppsett i prepod config (#227)
    * la til KAFKA_BROKERS oppsett i prepod config
    
    * oppdaterte kafka oppsett i application-preprod.yaml
    
    * fikset profil navn

--- src/main/kotlin/no/nav/familie/tilbake/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/familie/tilbake/config/KafkaConfig.kt
@@ -16,7 +16,7 @@ import org.springframework.kafka.core.ProducerFactory
 
 @Configuration
 @EnableKafka
-@Profile("prepod", "prod")
+@Profile("preprod", "prod")
 class KafkaConfig(@Value("\${KAFKA_BROKERS:localhost}") private val kafkaBrokers: String,
                   @Value("\${KAFKA_TRUSTSTORE_PATH}") private val kafkaTruststorePath: String,
                   @Value("\${KAFKA_CREDSTORE_PASSWORD}") private val kafkaCredstorePassword: String,
