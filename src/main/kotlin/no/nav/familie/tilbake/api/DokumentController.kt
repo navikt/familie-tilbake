f03accb83e23432806d4bb026255649959f7419d
ahandeland-nav <alexander.hadeland@nav.no>
2021-06-08 08:53:45 +0200

    Tester automatisk signing av commits

--- src/main/kotlin/no/nav/familie/tilbake/api/DokumentController.kt
+++ src/main/kotlin/no/nav/familie/tilbake/api/DokumentController.kt
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
-import java.math.BigDecimal
 import javax.validation.Valid
 
 @RestController
