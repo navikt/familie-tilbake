3ce5f89bcb462f5cbfd1fc46371a637046daa749
Saikat Halder <saikat.halder@nav.no>
2021-06-11 15:57:17 +0200

    konfigurerte ØkonomiConsumerConfig.kt med STS og deaktiverte CNCheck

--- src/main/kotlin/no/nav/familie/tilbake/config/ØkonomiConsumerConfig.kt
+++ src/main/kotlin/no/nav/familie/tilbake/config/ØkonomiConsumerConfig.kt
@@ -1,10 +1,15 @@
 package no.nav.familie.tilbake.config
 
+import no.nav.common.cxf.STSConfigurationUtil
+import no.nav.common.cxf.StsConfig
 import no.nav.okonomi.tilbakekrevingservice.TilbakekrevingPortType
+import org.apache.cxf.configuration.jsse.TLSClientParameters
 import org.apache.cxf.ext.logging.LoggingFeature
 import org.apache.cxf.ext.logging.LoggingInInterceptor
 import org.apache.cxf.ext.logging.LoggingOutInterceptor
+import org.apache.cxf.frontend.ClientProxy
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean
+import org.apache.cxf.transport.http.HTTPConduit
 import org.apache.cxf.ws.addressing.WSAddressingFeature
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -14,7 +19,12 @@ import javax.xml.namespace.QName
 
 @Configuration
 @Profile("!local")
-class ØkonomiConsumerConfig(@Value("\${TILBAKEKREVING_V1_URL}") private val tilbakekrevingUrl: String) {
+class ØkonomiConsumerConfig(
+        @Value("\${SECURITYTOKENSERVICE_URL}") private val stsUrl: String,
+        @Value("\${CREDENTIAL_USERNAME}") private val systemuserUsername: String,
+        @Value("\${CREDENTIAL_PASSWORD}") private val systemuserPwd: String,
+        @Value("\${TILBAKEKREVING_V1_URL}") private val tilbakekrevingUrl: String,
+) {
 
     private val WSDL = "wsdl/no/nav/tilbakekreving/tilbakekreving-v1-tjenestespesifikasjon.wsdl"
     private val NAMESPACE = "http://okonomi.nav.no/tilbakekrevingService/"
@@ -34,7 +44,9 @@ class ØkonomiConsumerConfig(@Value("\${TILBAKEKREVING_V1_URL}") private val til
             outInterceptors.add(LoggingOutInterceptor())
             inInterceptors.add(LoggingInInterceptor())
         }
-        return factoryBean.create(TilbakekrevingPortType::class.java)
+        return wrapWithSts(factoryBean.create(TilbakekrevingPortType::class.java)).apply {
+            disableCnCheck()
+        }
     }
 
     private fun loggingFeature(): LoggingFeature {
@@ -42,5 +54,23 @@ class ØkonomiConsumerConfig(@Value("\${TILBAKEKREVING_V1_URL}") private val til
         loggingFeature.setPrettyLogging(true)
         loggingFeature.setVerbose(true)
         return loggingFeature
+    }
+
+    private fun TilbakekrevingPortType.disableCnCheck() {
+        val client = ClientProxy.getClient(this)
+        val conduit = client.conduit as HTTPConduit
+        val tlsParams = TLSClientParameters()
+        tlsParams.isDisableCNCheck = true
+        conduit.tlsClientParameters = tlsParams
+    }
+
+    private fun wrapWithSts(port: TilbakekrevingPortType): TilbakekrevingPortType {
+        val client = ClientProxy.getClient(port)
+        val stsConfig = StsConfig.builder()
+                .url(stsUrl)
+                .username(systemuserUsername)
+                .password(systemuserPwd).build()
+        STSConfigurationUtil.configureStsForSystemUserInFSS(client, stsConfig)
+        return port
     }
 }
