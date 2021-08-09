80ab06c1c2f09227380f63bd2703030707a0bed6
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-08-09 13:32:49 +0200

    Midlertidig testendepunkt

---
+++ src/main/kotlin/no/nav/familie/tilbake/api/TestController.kt
@@ -0,0 +1,23 @@
+package no.nav.familie.tilbake.api
+
+import no.nav.familie.kontrakter.felles.saksbehandler.Saksbehandler
+import no.nav.familie.tilbake.integration.familie.IntegrasjonerClient
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/test")
+@ProtectedWithClaims(issuer = "azuread")
+class TestController(val integrasjonerClient: IntegrasjonerClient) {
+
+    @GetMapping(path = ["/{ident}"],
+                produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun sakjsbehandlernavn(ident: String): Saksbehandler {
+        return integrasjonerClient.hentSaksbehandler(ident)
+
+    }
+
+}
