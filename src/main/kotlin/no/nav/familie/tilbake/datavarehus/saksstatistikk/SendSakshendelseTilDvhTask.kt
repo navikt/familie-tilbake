c51a78b7346005f00da4a8a8778e349bb1c32742
Saikat Halder <45660476+haldersaikat@users.noreply.github.com>
2021-08-10 15:29:19 +0200

    la til logger for å logge hva som vi sender til Dvh. Endret Kafka konfig slik at listener sender ack etter å lese request (#346)
    * la til logger for å logge hva som vi sender til Dvh. Endret Kafka konfig slik at listener sender ack etter å lese request
    
    * oppdaterte til securelog

--- src/main/kotlin/no/nav/familie/tilbake/datavarehus/saksstatistikk/SendSakshendelseTilDvhTask.kt
+++ src/main/kotlin/no/nav/familie/tilbake/datavarehus/saksstatistikk/SendSakshendelseTilDvhTask.kt
@@ -7,6 +7,7 @@ import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.tilbake.datavarehus.saksstatistikk.sakshendelse.Behandlingstilstand
 import no.nav.familie.tilbake.integration.kafka.KafkaProducer
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
@@ -17,7 +18,10 @@ import java.util.UUID
                      beskrivelse = "Sending av sakshendelser til datavarehus")
 class SendSakshendelseTilDvhTask(private val kafkaProducer: KafkaProducer) : AsyncTaskStep {
 
+    private val log = LoggerFactory.getLogger(this::class.java)
+
     override fun doTask(task: Task) {
+        log.info("SendSakshendelseTilDvhTask prosesserer med id=${task.id} og metadata ${task.metadata}")
         val behandlingId = UUID.fromString(task.payload)
         val behandlingstilstand: Behandlingstilstand = objectMapper.readValue(task.metadata.getProperty("behandlingstilstand"))
         kafkaProducer.sendSaksdata(behandlingId,
