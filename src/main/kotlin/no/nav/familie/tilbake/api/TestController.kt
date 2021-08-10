a0438f5eef4399c5035f5a0e9d1c1136394ba059
Bent Lorentzen <bent.lorentzen@nav.no>
2021-08-10 14:13:09 +0200

    modified

--- src/main/kotlin/no/nav/familie/tilbake/api/TestController.kt
+++ src/main/kotlin/no/nav/familie/tilbake/api/TestController.kt
@@ -3,6 +3,7 @@ package no.nav.familie.tilbake.api
 import no.nav.familie.kontrakter.felles.saksbehandler.Saksbehandler
 import no.nav.familie.tilbake.integration.familie.IntegrasjonerClient
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/test")
 @ProtectedWithClaims(issuer = "azuread")
+//@Unprotected
 class TestController(val integrasjonerClient: IntegrasjonerClient) {
 
     @GetMapping(path = ["/{ident}"],
