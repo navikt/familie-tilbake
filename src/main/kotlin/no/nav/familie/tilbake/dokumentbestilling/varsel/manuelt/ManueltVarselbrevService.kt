ff0023ba598bb7e59ccd0cb2247a3b025f99a3dc
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-08-26 08:36:43 +0200

    Fikset brukket test.

--- src/main/kotlin/no/nav/familie/tilbake/dokumentbestilling/varsel/manuelt/ManueltVarselbrevService.kt
+++ src/main/kotlin/no/nav/familie/tilbake/dokumentbestilling/varsel/manuelt/ManueltVarselbrevService.kt
@@ -6,7 +6,6 @@ import no.nav.familie.tilbake.behandling.domain.Behandling
 import no.nav.familie.tilbake.behandling.domain.Fagsak
 import no.nav.familie.tilbake.behandling.domain.Varsel
 import no.nav.familie.tilbake.common.repository.findByIdOrThrow
-import no.nav.familie.tilbake.faktaomfeilutbetaling.FaktaFeilutbetalingService
 import no.nav.familie.tilbake.dokumentbestilling.brevmaler.Dokumentmalstype
 import no.nav.familie.tilbake.dokumentbestilling.felles.Adresseinfo
 import no.nav.familie.tilbake.dokumentbestilling.felles.Brevmottager
@@ -19,6 +18,7 @@ import no.nav.familie.tilbake.dokumentbestilling.fritekstbrev.Fritekstbrevsdata
 import no.nav.familie.tilbake.dokumentbestilling.varsel.TekstformatererVarselbrev
 import no.nav.familie.tilbake.dokumentbestilling.varsel.VarselbrevUtil
 import no.nav.familie.tilbake.dokumentbestilling.varsel.handlebars.dto.Varselbrevsdokument
+import no.nav.familie.tilbake.faktaomfeilutbetaling.FaktaFeilutbetalingService
 import org.springframework.stereotype.Service
 import java.util.UUID
 
@@ -38,7 +38,7 @@ class ManueltVarselbrevService(private val behandlingRepository: BehandlingRepos
                                                           brevmottager,
                                                           false,
                                                           behandling.aktivtVarsel)
-        val data = lagManueltVarselBrev(varselbrevsdokument)
+        val data = lagKorrigertVarselbrev(varselbrevsdokument)
         val varsletFeilutbetaling = varselbrevsdokument.beløp
         pdfBrevService.sendBrev(behandling,
                                 fagsak,
