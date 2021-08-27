fd898cda0d76598bde9c0886885014f590c1d5f9
haldersaikat <saikat.halder@nav.no>
2021-08-27 13:21:55 +0200

    implementerte behandlingskontroll for verge

---
+++ src/main/kotlin/no/nav/familie/tilbake/behandling/steg/Vergessteg.kt
@@ -0,0 +1,58 @@
+package no.nav.familie.tilbake.behandling.steg
+
+import no.nav.familie.tilbake.api.dto.BehandlingsstegDto
+import no.nav.familie.tilbake.api.dto.BehandlingsstegVergeDto
+import no.nav.familie.tilbake.behandling.BehandlingRepository
+import no.nav.familie.tilbake.behandling.VergeService
+import no.nav.familie.tilbake.behandlingskontroll.BehandlingskontrollService
+import no.nav.familie.tilbake.behandlingskontroll.Behandlingsstegsinfo
+import no.nav.familie.tilbake.behandlingskontroll.domain.Behandlingssteg
+import no.nav.familie.tilbake.behandlingskontroll.domain.Behandlingsstegstatus
+import no.nav.familie.tilbake.behandlingskontroll.domain.Behandlingsstegstatus.AUTOUTFØRT
+import no.nav.familie.tilbake.behandlingskontroll.domain.Behandlingsstegstatus.UTFØRT
+import no.nav.familie.tilbake.common.repository.findByIdOrThrow
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
+
+@Service
+class Vergessteg(private val behandlingRepository: BehandlingRepository,
+                 private val vergeService: VergeService,
+                 private val behandlingskontrollService: BehandlingskontrollService) : IBehandlingssteg {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @Transactional
+    override fun utførSteg(behandlingId: UUID) {
+        logger.info("Behandling $behandlingId er på ${Behandlingssteg.VERGE} steg")
+        val behandling = behandlingRepository.findByIdOrThrow(behandlingId)
+        if (behandling.harVerge) {
+            behandlingskontrollService.oppdaterBehandlingsstegsstaus(behandlingId, Behandlingsstegsinfo(Behandlingssteg.VERGE,
+                                                                                                        AUTOUTFØRT))
+        }
+        behandlingskontrollService.fortsettBehandling(behandlingId)
+    }
+
+    @Transactional
+    override fun utførSteg(behandlingId: UUID, behandlingsstegDto: BehandlingsstegDto) {
+        logger.info("Behandling $behandlingId er på ${Behandlingssteg.VERGE} steg")
+        vergeService.lagreVerge(behandlingId, (behandlingsstegDto as BehandlingsstegVergeDto).verge)
+        behandlingskontrollService.oppdaterBehandlingsstegsstaus(behandlingId, Behandlingsstegsinfo(Behandlingssteg.VERGE,
+                                                                                                    UTFØRT))
+        behandlingskontrollService.fortsettBehandling(behandlingId)
+    }
+
+    @Transactional
+    override fun gjenopptaSteg(behandlingId: UUID) {
+        logger.info("Behandling $behandlingId gjenopptar på ${Behandlingssteg.VERGE} steg")
+        behandlingskontrollService.oppdaterBehandlingsstegsstaus(behandlingId,
+                                                                 Behandlingsstegsinfo(Behandlingssteg.VERGE,
+                                                                                      Behandlingsstegstatus.KLAR))
+    }
+
+    override fun getBehandlingssteg(): Behandlingssteg {
+        return Behandlingssteg.VERGE
+    }
+
+}
