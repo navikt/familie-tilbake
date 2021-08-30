86ffc2e5002b61de407ee2b02bb4fbe40484348f
haldersaikat <saikat.halder@nav.no>
2021-08-30 09:36:30 +0200

    oppdatert database skript versjon

--- src/test/kotlin/no/nav/familie/tilbake/DatabaseChangesTest.kt
+++ src/test/kotlin/no/nav/familie/tilbake/DatabaseChangesTest.kt
@@ -10,7 +10,7 @@ class DatabaseChangesTest {
     companion object {
 
         // Denne knekker bygg med høyere db-versjon enn main. Oppdater kun når du er klar for å merge db-endringer.
-        const val MERGED_DB_VERSION = 20
+        const val MERGED_DB_VERSION = 21
     }
 
     /**
