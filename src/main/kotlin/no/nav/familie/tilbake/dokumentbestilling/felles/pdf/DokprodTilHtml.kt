14d231e5639b2d5f8a9cbbec9268f4bfc7010534
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-11-19 16:10:36 +0100

    Fikset escaping av tegn i handlebars.

--- src/main/kotlin/no/nav/familie/tilbake/dokumentbestilling/felles/pdf/DokprodTilHtml.kt
+++ src/main/kotlin/no/nav/familie/tilbake/dokumentbestilling/felles/pdf/DokprodTilHtml.kt
@@ -3,9 +3,7 @@ package no.nav.familie.tilbake.dokumentbestilling.felles.pdf
 object DokprodTilHtml {
 
     fun dokprodInnholdTilHtml(tekst: String): String {
-        val preSanitized = reddUnicodeMarkerFraSanitize(tekst)
-        val sanitized = sanitize(preSanitized)
-        val dokprod = leggTilbakeUnicodeMarker(sanitized)
+        val dokprod: String = sanitize(tekst)
         val builder = StringBuilder()
         val avsnittene = hentAvsnittene(dokprod)
         var samepageStarted = false
@@ -83,14 +81,6 @@ object DokprodTilHtml {
             }
         }
         return ekstraLinjeskiftFørHilsing(konverterNbsp(builder.toString()))
-    }
-
-    private fun reddUnicodeMarkerFraSanitize(tekst: String): String {
-        return tekst.replace("&#", "ampersandHash")
-    }
-
-    private fun leggTilbakeUnicodeMarker(tekst: String): String {
-        return tekst.replace("ampersandHash", "&#")
     }
 
     private fun sanitize(name: String): String {
