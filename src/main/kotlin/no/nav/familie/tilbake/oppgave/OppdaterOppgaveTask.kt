a0d8e8c2c16c84a8913925d01698129c3b0a9548
Geir Roger Moen <geir.roger.moen@nav.no>
2021-12-15 10:50:12 +0100

    Spesialhåndtering for 2 task i e2e. Taskene spammer loggen unødvendig

--- src/main/kotlin/no/nav/familie/tilbake/oppgave/OppdaterOppgaveTask.kt
+++ src/main/kotlin/no/nav/familie/tilbake/oppgave/OppdaterOppgaveTask.kt
@@ -4,6 +4,7 @@ import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
 import org.slf4j.LoggerFactory
+import org.springframework.core.env.Environment
 import org.springframework.stereotype.Service
 import java.util.UUID
 
@@ -13,12 +14,15 @@ import java.util.UUID
                      maxAntallFeil = 3,
                      beskrivelse = "Oppdaterer oppgave",
                      triggerTidVedFeilISekunder = 300L)
-class OppdaterOppgaveTask(private val oppgaveService: OppgaveService) : AsyncTaskStep {
+class OppdaterOppgaveTask(private val oppgaveService: OppgaveService,
+                          val environment: Environment) : AsyncTaskStep {
 
     private val log = LoggerFactory.getLogger(this::class.java)
 
     override fun doTask(task: Task) {
         log.info("OppdaterOppgaveTask prosesserer med id=${task.id} og metadata ${task.metadata}")
+        if (environment.activeProfiles.contains("e2e")) return
+
         val frist = task.metadata.getProperty("frist")
         val beskrivelse = task.metadata.getProperty("beskrivelse")
         val behandlingId = UUID.fromString(task.payload)
