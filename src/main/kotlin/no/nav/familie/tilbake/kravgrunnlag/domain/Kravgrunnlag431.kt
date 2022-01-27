adf144fe29482dff645091c598fc59461524115e
haldersaikat <saikat.halder@nav.no>
2022-01-27 13:36:06 +0100

    La til just klassekode for EF KL_KODE_JUST_EFOG

--- src/main/kotlin/no/nav/familie/tilbake/kravgrunnlag/domain/Kravgrunnlag431.kt
+++ src/main/kotlin/no/nav/familie/tilbake/kravgrunnlag/domain/Kravgrunnlag431.kt
@@ -129,6 +129,7 @@ enum class Klassekode(val aktivitet: String) {
     KL_KODE_FEIL_EFOG(""),
     KL_KODE_FEIL_PEN(""),
     KL_KODE_JUST_BA(""),
+    KL_KODE_JUST_EFOG(""),
     BATR("Barnetrygd"),
     BATRSMA("Småbarnstillegg"),
     EFOG("Overgangsstønad"),
