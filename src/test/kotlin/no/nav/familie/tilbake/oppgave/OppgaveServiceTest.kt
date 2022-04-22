299bef7a911cf11c81330a9975310e2e4e6dee3c
haldersaikat <saikat.halder@nav.no>
2022-04-22 10:26:58 +0200

    Sjekker om det allerede finnes en oppgave eller ligger en ferdigstill oppgave før å opprette oppgave for å unngå å opperette flere oppgaver for samme type og samme behandling

--- src/test/kotlin/no/nav/familie/tilbake/oppgave/OppgaveServiceTest.kt
+++ src/test/kotlin/no/nav/familie/tilbake/oppgave/OppgaveServiceTest.kt
@@ -1,14 +1,20 @@
 package no.nav.familie.tilbake.oppgave
 
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import io.mockk.CapturingSlot
 import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import no.nav.familie.kontrakter.felles.oppgave.FinnOppgaveResponseDto
 import no.nav.familie.kontrakter.felles.oppgave.MappeDto
+import no.nav.familie.kontrakter.felles.oppgave.Oppgave
 import no.nav.familie.kontrakter.felles.oppgave.Oppgavetype
 import no.nav.familie.kontrakter.felles.oppgave.OpprettOppgaveRequest
+import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.domene.TaskRepository
 import no.nav.familie.tilbake.behandling.BehandlingRepository
 import no.nav.familie.tilbake.behandling.FagsakRepository
 import no.nav.familie.tilbake.common.repository.findByIdOrThrow
@@ -21,6 +27,7 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.core.env.Environment
 import java.time.LocalDate
+import java.util.Properties
 
 
 class OppgaveServiceTest {
@@ -30,6 +37,7 @@ class OppgaveServiceTest {
     private val integrasjonerClient: IntegrasjonerClient = mockk(relaxed = true)
     private val personService: PersonService = mockk(relaxed = true)
     private val environment: Environment = mockk(relaxed = true)
+    private val taskRepository: TaskRepository = mockk(relaxed = true)
 
 
     private val mappeIdGodkjenneVedtak = 100
@@ -46,10 +54,13 @@ class OppgaveServiceTest {
                                         fagsakRepository,
                                         integrasjonerClient,
                                         personService,
-                                        environment)
+                                        taskRepository,
+                                        environment,)
         every { fagsakRepository.findByIdOrThrow(fagsak.id) } returns fagsak
         every { behandlingRepository.findByIdOrThrow(behandling.id) } returns behandling
         every { integrasjonerClient.finnMapper(any()) } returns finnMappeResponseDto
+        every { integrasjonerClient.finnOppgaver(any()) } returns FinnOppgaveResponseDto(0L, emptyList())
+        every { taskRepository.findByStatusInAndType(any(),any(),any()) } returns emptyList()
     }
 
     @Nested
@@ -132,6 +143,48 @@ class OppgaveServiceTest {
             verify { integrasjonerClient.opprettOppgave(capture(slot)) }
 
             slot.captured.mappeId shouldBe null
+        }
+
+        @Test
+        fun `skal ikke legge godkjenneVedtak oppgaver i EF-Sak-50-mappe når det allerede finnes en`() {
+            every { integrasjonerClient.finnMapper("4489") } returns finnMappeResponseDto
+            every { integrasjonerClient.finnOppgaver(any()) } returns FinnOppgaveResponseDto(1L, listOf(Oppgave()))
+
+            val exception = shouldThrow<RuntimeException> {
+                oppgaveService.opprettOppgave(behandling.id,
+                                              Oppgavetype.GodkjenneVedtak,
+                                              "4483",
+                                              "",
+                                              LocalDate.now().plusDays(5),
+                                              "bob")
+            }
+            exception.message shouldBe "Det finnes allerede en oppgave ${Oppgavetype.GodkjenneVedtak} " +
+                    "for behandling ${behandling.id} og finnes ikke noen ferdigstilleoppgaver. " +
+                    "Eksisterende oppgaven ${Oppgavetype.GodkjenneVedtak} må lukke først."
+        }
+
+        @Test
+        fun `skal legge godkjenneVedtak oppgaver når det allerede finnes en og har en åpen ferdigstilloppgave task`() {
+            val slot = CapturingSlot<OpprettOppgaveRequest>()
+
+            every { integrasjonerClient.finnMapper("4489") } returns finnMappeResponseDto
+            every { integrasjonerClient.finnOppgaver(any()) } returns FinnOppgaveResponseDto(1L, listOf(Oppgave()))
+            val properties = Properties().apply { setProperty("oppgavetype", Oppgavetype.GodkjenneVedtak.name) }
+            every { taskRepository.findByStatusInAndType(any(), any(), any()) } returns
+                    listOf(Task(type = FerdigstillOppgaveTask.TYPE, payload = behandling.id.toString(), properties = properties))
+
+            shouldNotThrow<RuntimeException> {
+                oppgaveService.opprettOppgave(behandling.id,
+                                              Oppgavetype.GodkjenneVedtak,
+                                              "4483",
+                                              "",
+                                              LocalDate.now().plusDays(5),
+                                              "bob")
+            }
+
+            verify { integrasjonerClient.opprettOppgave(capture(slot)) }
+
+            slot.captured.mappeId shouldBe mappeIdGodkjenneVedtak
         }
     }
 }
