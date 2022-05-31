308aa2ec40d38d8980e01299264df92ee8d5c1af
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2022-05-31 10:40:37 +0200

    Fikset feil hjemmelshenvisning (#738)

--- src/main/kotlin/no/nav/familie/tilbake/dokumentbestilling/innhentdokumentasjon/handlebars/dto/InnhentDokumentasjonsbrevsdokument.kt
+++ src/main/kotlin/no/nav/familie/tilbake/dokumentbestilling/innhentdokumentasjon/handlebars/dto/InnhentDokumentasjonsbrevsdokument.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.tilbake.dokumentbestilling.innhentdokumentasjon.handlebars.dto
 
+import no.nav.familie.kontrakter.felles.tilbakekreving.Ytelsestype
 import no.nav.familie.tilbake.dokumentbestilling.felles.Brevmetadata
 import no.nav.familie.tilbake.dokumentbestilling.felles.BrevmottagerUtil
 import no.nav.familie.tilbake.dokumentbestilling.handlebars.dto.BaseDokument
@@ -16,6 +17,10 @@ data class InnhentDokumentasjonsbrevsdokument(val brevmetadata: Brevmetadata,
     val finnesVerge: Boolean = brevmetadata.finnesVerge
 
     val annenMottagersNavn: String? = BrevmottagerUtil.getannenMottagersNavn(brevmetadata)
+
+    @Suppress("unused") // Handlebars
+    val isRentepliktig
+        get() = ytelsestype != Ytelsestype.BARNETRYGD
 
     init {
         if (finnesVerge) {
