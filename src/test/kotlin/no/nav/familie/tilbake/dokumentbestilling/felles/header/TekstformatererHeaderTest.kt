1616cfd9be1c1ade1555c38d5a6a62fb1e84f46e
Geir Roger Moen <geir.roger.moen@nav.no>
2022-09-22 12:33:12 +0200

    Fikset kompileringsfeil

--- src/test/kotlin/no/nav/familie/tilbake/dokumentbestilling/felles/header/TekstformatererHeaderTest.kt
+++ src/test/kotlin/no/nav/familie/tilbake/dokumentbestilling/felles/header/TekstformatererHeaderTest.kt
@@ -1,22 +1,16 @@
 package no.nav.familie.tilbake.dokumentbestilling.felles.header
 
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockk
 import no.nav.familie.kontrakter.felles.Språkkode
 import no.nav.familie.kontrakter.felles.tilbakekreving.Ytelsestype
 import no.nav.familie.tilbake.behandling.domain.Institusjon
 import no.nav.familie.tilbake.dokumentbestilling.felles.Adresseinfo
 import no.nav.familie.tilbake.dokumentbestilling.felles.Brevmetadata
-import no.nav.familie.tilbake.organisasjon.OrganisasjonService
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class TekstformatererHeaderTest {
-
-    private val organisasjonService: OrganisasjonService = mockk()
 
     private val brevmetadata = Brevmetadata(
         sakspartId = "12345678901",
@@ -31,16 +25,9 @@ class TekstformatererHeaderTest {
         gjelderDødsfall = false
     )
 
-    @BeforeAll
-    fun setup() {
-        every {
-            organisasjonService.hentOrganisasjonNavn(any())
-        } answers { "Testinstitusjon" }
-    }
-
     @Test
     fun `lagHeader brev til bruker`() {
-        val generertHeader: String = TekstformatererHeader.lagHeader(brevmetadata, "Dette er en header", organisasjonService)
+        val generertHeader: String = TekstformatererHeader.lagHeader(brevmetadata, "Dette er en header", null)
         generertHeader shouldBe personHeader()
     }
 
@@ -50,7 +37,7 @@ class TekstformatererHeaderTest {
             TekstformatererHeader.lagHeader(
                 brevmetadata = brevmetadata.copy(institusjon = Institusjon("987654321")),
                 overskrift = "Dette er en header",
-                organisasjonService = organisasjonService
+                organisasjonsnavn = "Testinstitusjon"
             )
         generertHeader shouldBe institusjonHeader()
     }
