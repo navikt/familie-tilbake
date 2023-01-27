32315db2d1f7a383a2a3bb9fdbf78f8da468cd64
Johan Blomgren <blommish@users.noreply.github.com>
2023-01-27 12:37:56 +0100

    håndterSteg skal skje i en transacation sånn at allt blir rullet tilbake hvis feil intreffer

--- src/main/kotlin/no/nav/familie/tilbake/behandling/steg/StegService.kt
+++ src/main/kotlin/no/nav/familie/tilbake/behandling/steg/StegService.kt
@@ -11,6 +11,7 @@ import no.nav.familie.tilbake.common.exceptionhandler.Feil
 import no.nav.familie.tilbake.common.repository.findByIdOrThrow
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
 
 @Service
@@ -20,6 +21,7 @@ class StegService(
     val behandlingskontrollService: BehandlingskontrollService
 ) {
 
+    @Transactional
     fun håndterSteg(behandlingId: UUID) {
         var aktivtBehandlingssteg: Behandlingssteg = hentAktivBehandlingssteg(behandlingId)
 
@@ -32,6 +34,7 @@ class StegService(
         }
     }
 
+    @Transactional
     fun håndterSteg(behandlingId: UUID, behandlingsstegDto: BehandlingsstegDto) {
         val behandling = behandlingRepository.findByIdOrThrow(behandlingId)
         if (behandling.erSaksbehandlingAvsluttet) {
@@ -73,6 +76,7 @@ class StegService(
         }
     }
 
+    @Transactional
     fun håndterStegAutomatisk(behandlingId: UUID) {
         val behandling = behandlingRepository.findByIdOrThrow(behandlingId)
         if (behandling.erSaksbehandlingAvsluttet) {
@@ -101,6 +105,7 @@ class StegService(
         }
     }
 
+    @Transactional
     fun gjenopptaSteg(behandlingId: UUID) {
         var aktivtBehandlingssteg = hentAktivBehandlingssteg(behandlingId)
 
