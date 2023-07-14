c69b40e007a8e2da91cf1c01ec857c09e3d3eba9
marius-nav <Marius.Glad.Fredriksen@nav.no>
2023-07-14 10:40:59 +0200

    NAV-13885 - Lagt til validering for at det ikke er tillatt med manuelle brevmottakere når behandlingen inneholder fortrolig person (person med adressebeskyttelse STRENGT_FORTROLIG / STRENGT_FORTROLIG_UTLAND) i “leggTilBrevmottaker” og i “opprettBrevmottakerSteg”.

--- src/main/kotlin/no/nav/familie/tilbake/behandling/ValiderBrevmottakerService.kt
+++ src/main/kotlin/no/nav/familie/tilbake/behandling/ValiderBrevmottakerService.kt
@@ -1,19 +1,19 @@
 package no.nav.familie.tilbake.behandling
 
 import no.nav.familie.tilbake.common.exceptionhandler.Feil
-import no.nav.familie.tilbake.dokumentbestilling.manuell.brevmottaker.ManuellBrevmottakerService
+import no.nav.familie.tilbake.dokumentbestilling.manuell.brevmottaker.ManuellBrevmottakerRepository
 import no.nav.familie.tilbake.person.PersonService
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
 class ValiderBrevmottakerService(
-    private val manuellBrevmottakerService: ManuellBrevmottakerService,
+    private val manuellBrevmottakerRepository: ManuellBrevmottakerRepository,
     private val fagsakService: FagsakService,
     private val personService: PersonService
 ) {
     fun validerAtBehandlingIkkeInneholderStrengtFortroligPersonMedManuelleBrevmottakere(behandlingId: UUID, fagsakId: UUID) {
-        val manuelleBrevmottakere = manuellBrevmottakerService.hentBrevmottakere(behandlingId).takeIf { it.isNotEmpty() } ?: return
+        val manuelleBrevmottakere = manuellBrevmottakerRepository.findByBehandlingId(behandlingId).takeIf { it.isNotEmpty() } ?: return
         val fagsak = fagsakService.hentFagsak(fagsakId)
         val bruker = fagsak.bruker
         val fagsystem = fagsak.fagsystem
@@ -23,6 +23,22 @@ class ValiderBrevmottakerService(
         if (strengtFortroligePersonIdenter.isNotEmpty()) {
             val melding =
                 "Behandlingen (id: $behandlingId) inneholder person med strengt fortrolig adressebeskyttelse og kan ikke kombineres med manuelle brevmottakere (${manuelleBrevmottakere.size} stk)."
+            val frontendFeilmelding =
+                "Behandlingen inneholder person med strengt fortrolig adressebeskyttelse og kan ikke kombineres med manuelle brevmottakere."
+            throw Feil(melding, frontendFeilmelding)
+        }
+    }
+
+    fun validerAtBehandlingenIkkeInneholderStrengtFortroligPerson(behandlingId: UUID, fagsakId: UUID) {
+        val fagsak = fagsakService.hentFagsak(fagsakId)
+        val bruker = fagsak.bruker
+        val fagsystem = fagsak.fagsystem
+        val personIdenter = listOfNotNull(bruker.ident)
+        if (personIdenter.isEmpty()) return
+        val strengtFortroligePersonIdenter = personService.hentIdenterMedStrengtFortroligAdressebeskyttelse(personIdenter, fagsystem)
+        if (strengtFortroligePersonIdenter.isNotEmpty()) {
+            val melding =
+                "Behandlingen (id: $behandlingId) inneholder person med strengt fortrolig adressebeskyttelse og kan ikke kombineres med manuelle brevmottakere."
             val frontendFeilmelding =
                 "Behandlingen inneholder person med strengt fortrolig adressebeskyttelse og kan ikke kombineres med manuelle brevmottakere."
             throw Feil(melding, frontendFeilmelding)
