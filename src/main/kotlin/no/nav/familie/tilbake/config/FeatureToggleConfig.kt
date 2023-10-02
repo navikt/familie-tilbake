0b7e9ecef169d2039a05d5006e21a40dd3226e80
throndi <56258085+throndi@users.noreply.github.com>
2023-10-02 15:25:27 +0200

    Legger til controller for testing av featuretoggles

--- src/main/kotlin/no/nav/familie/tilbake/config/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/tilbake/config/FeatureToggleConfig.kt
@@ -2,11 +2,16 @@ package no.nav.familie.tilbake.config
 
 import io.getunleash.strategy.Strategy
 import no.nav.familie.unleash.UnleashService
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.awt.PageAttributes
 
 @Configuration
 class FeatureToggleConfig(@Value("\${NAIS_CLUSTER_NAME}") private val clusterName: String) {
@@ -40,6 +45,27 @@ class FeatureToggleService(val unleashService: UnleashService) {
 
     fun isEnabled(toggleId: String, defaultValue: Boolean): Boolean {
         return unleashService.isEnabled(toggleId, defaultValue)
+    }
+}
+
+/**
+ * TODO : Fjern etter testing er utført.
+ */
+@RestController
+@RequestMapping(path = ["/api/featuretoggle"])
+@Unprotected
+class FeatureToggleController(private val featureToggleService: FeatureToggleService) {
+
+    private val funksjonsbrytere: Set<String> = setOf(
+        FeatureToggleConfig.BRUK_6_DESIMALER_I_SKATTEBEREGNING,
+        FeatureToggleConfig.KAN_OPPRETTE_BEH_MED_EKSTERNID_SOM_HAR_AVSLUTTET_TBK,
+        FeatureToggleConfig.OVERSTYR_DELVILS_TILBAKEKREVING_TIL_FULL_TILBAKEKREVING,
+        FeatureToggleConfig.IKKE_VALIDER_SÆRLIG_GRUNNET_ANNET_FRITEKST,
+    )
+
+    @GetMapping
+    fun sjekkAlle(): Map<String, Boolean> {
+        return funksjonsbrytere.associate { it to featureToggleService.isEnabled(it) }
     }
 }
 
