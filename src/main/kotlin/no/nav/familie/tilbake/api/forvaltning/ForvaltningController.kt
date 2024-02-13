d140b3f2910378b0cbe4301f062996e0adc8576e
Stig Helge Strøm <stig.helge.strom@nav.no>
2024-02-13 13:45:13 +0100

    ktlint

--- src/main/kotlin/no/nav/familie/tilbake/api/forvaltning/ForvaltningController.kt
+++ src/main/kotlin/no/nav/familie/tilbake/api/forvaltning/ForvaltningController.kt
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation
 import no.nav.familie.kontrakter.felles.Fagsystem
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.tilbakekreving.Ytelsestype
-import no.nav.familie.tilbake.behandling.domain.Iverksettingsstatus
 import no.nav.familie.tilbake.forvaltning.ForvaltningService
 import no.nav.familie.tilbake.oppgave.OppgaveTaskService
 import no.nav.familie.tilbake.sikkerhet.AuditLoggerEvent
@@ -165,14 +164,12 @@ class ForvaltningController(
         return Ressurs.success(forvaltningService.hentForvaltningsinfo(ytelsestype, eksternFagsakId))
     }
 
-
     @Operation(summary = "Hent locale og klokkeslett")
     @GetMapping(
         path = ["/hentlocale"],
         produces = [MediaType.APPLICATION_JSON_VALUE],
     )
-    fun hentlocale(
-    ): Ressurs<String> {
+    fun hentlocale(): Ressurs<String> {
         return Ressurs.success(LocaleContextHolder.getLocale().toString() + " " + ZonedDateTime.now())
     }
 
@@ -193,7 +190,8 @@ class ForvaltningController(
         produces = [MediaType.APPLICATION_JSON_VALUE],
     )
     fun settIverksettStegTilUtførtOgFortsett(
-        @PathVariable taskId: Long, @PathVariable behandlingId: UUID,
+        @PathVariable taskId: Long,
+        @PathVariable behandlingId: UUID,
     ) {
         forvaltningService.hoppOverIverksettingMotOppdrag(behandlingId = behandlingId, taskId = taskId)
     }
