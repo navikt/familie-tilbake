663c0c6fe5522ee51fbaad475099a18eefedc6d0
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2024-05-09 18:12:38 +0200

    Legg til rolle tilgangssjekk på endepunkt

--- src/main/kotlin/no/nav/familie/tilbake/api/HistorikkController.kt
+++ src/main/kotlin/no/nav/familie/tilbake/api/HistorikkController.kt
@@ -4,6 +4,10 @@ import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.tilbake.api.dto.HistorikkinnslagDto
 import no.nav.familie.tilbake.api.dto.tilDto
 import no.nav.familie.tilbake.historikkinnslag.HistorikkService
+import no.nav.familie.tilbake.sikkerhet.AuditLoggerEvent
+import no.nav.familie.tilbake.sikkerhet.Behandlerrolle
+import no.nav.familie.tilbake.sikkerhet.HenteParam
+import no.nav.familie.tilbake.sikkerhet.Rolletilgangssjekk
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
 import org.springframework.validation.annotation.Validated
@@ -21,6 +25,12 @@ class HistorikkController(private val historikkService: HistorikkService) {
     @GetMapping(
         "/{behandlingId}/historikk",
         produces = [MediaType.APPLICATION_JSON_VALUE],
+    )
+    @Rolletilgangssjekk(
+        minimumBehandlerrolle = Behandlerrolle.VEILEDER,
+        handling = "Henter historikkinnslag",
+        AuditLoggerEvent.ACCESS,
+        henteParam = HenteParam.BEHANDLING_ID,
     )
     fun hentHistorikkinnslag(
         @PathVariable("behandlingId") behandlingId: UUID,
