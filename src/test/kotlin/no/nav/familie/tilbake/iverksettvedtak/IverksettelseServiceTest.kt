1fae8a2d0eab3e2e93976e95c466f56e78b365a7
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2024-05-23 14:40:29 +0200

    Ikke iverksett to ganger (#1443)
    Co-authored-by: ma10s <mattis.janitz@nav.no>

--- src/test/kotlin/no/nav/familie/tilbake/iverksettvedtak/IverksettelseServiceTest.kt
+++ src/test/kotlin/no/nav/familie/tilbake/iverksettvedtak/IverksettelseServiceTest.kt
@@ -56,6 +56,8 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.test.context.transaction.TestTransaction
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.client.RestOperations
 import java.math.BigDecimal
 import java.math.BigInteger
@@ -142,19 +144,7 @@ internal class IverksettelseServiceTest : OppslagSpringRunnerTest() {
 
     @Test
     fun `sendIverksettVedtak skal sende iverksettvedtak til økonomi for suksess respons`() {
-        wireMockServer.stubFor(
-            WireMock.post(WireMock.urlEqualTo("/${DefaultOppdragClient.IVERKSETTELSE_PATH}/$behandlingId"))
-                .willReturn(
-                    WireMock.okJson(
-                        Ressurs.success(
-                            lagRespons(
-                                "00",
-                                "OK",
-                            ),
-                        ).toJson(),
-                    ),
-                ),
-        )
+        mockIverksettelseResponse("00", "OK")
 
         iverksettelseService.sendIverksettVedtak(behandlingId)
 
@@ -171,30 +161,44 @@ internal class IverksettelseServiceTest : OppslagSpringRunnerTest() {
 
     @Test
     fun `sendIverksettVedtak skal sende iverksettvedtak til økonomi for feil respons`() {
-        wireMockServer.stubFor(
-            WireMock.post(WireMock.urlEqualTo("/${DefaultOppdragClient.IVERKSETTELSE_PATH}/$behandlingId"))
-                .willReturn(
-                    WireMock.okJson(
-                        Ressurs.success(
-                            lagRespons(
-                                "10",
-                                "feil",
-                            ),
-                        ).toJson(),
-                    ),
-                ),
-        )
+        mockIverksettelseResponse("10", "feil")
 
         val exception = shouldThrow<RuntimeException> { iverksettelseService.sendIverksettVedtak(behandlingId) }
+
         exception.shouldBeInstanceOf<IntegrasjonException>()
         exception.message shouldBe "Noe gikk galt ved iverksetting av behandling=$behandlingId"
         exception.cause!!.message shouldBe "Fikk feil respons fra økonomi ved iverksetting av behandling=$behandlingId." +
             "Mottatt respons:${objectMapper.writeValueAsString(lagMmmelDto("10", "feil"))}"
 
         val økonomiXmlSendt = økonomiXmlSendtRepository.findByBehandlingId(behandlingId)
-        økonomiXmlSendt.shouldNotBeNull()
-        assertRequestXml(økonomiXmlSendt.melding, behandlingId, økonomiXmlSendt.id)
-        økonomiXmlSendt.kvittering.shouldBeNull()
+        økonomiXmlSendt.shouldBeNull()
+    }
+
+    @Test
+    fun `sendIverksettVedtak for allerede iverksatt behandling - skal returnere KVITTERING_OK og ikke iverksette`() {
+        mockIverksettelseResponse("08", "B441012F") // Denne kan håndteres dersom oppdrag skiller på om vedtak finnes eller om det er feil status
+
+        val exception = shouldThrow<RuntimeException> { iverksettelseService.sendIverksettVedtak(behandlingId) }
+        exception.shouldBeInstanceOf<IntegrasjonException>()
+    }
+
+    private fun mockIverksettelseResponse(
+        alvorlighetsgrad: String,
+        kodeMelding: String,
+    ) {
+        wireMockServer.stubFor(
+            WireMock.post(WireMock.urlEqualTo("/${DefaultOppdragClient.IVERKSETTELSE_PATH}/$behandlingId"))
+                .willReturn(
+                    WireMock.okJson(
+                        Ressurs.success(
+                            lagRespons(
+                                alvorlighetsgrad,
+                                kodeMelding,
+                            ),
+                        ).toJson(),
+                    ),
+                ),
+        )
     }
 
     private fun lagKravgrunnlag(): Kravgrunnlag431 {
