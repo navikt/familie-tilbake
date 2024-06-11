81594e33ffaa7232be09a6d123b8e6c9c3e9528c
thoalm <thoalm@outlook.com>
2024-06-11 12:00:49 +0200

    NAV-21086: Kravgrunnlag på samme eksterne fagsak id skal nå får spredt triggertid for å unngå oppretting flere opprettinger av behandliner for samme fagsak

--- src/main/kotlin/no/nav/familie/tilbake/kravgrunnlag/batch/HåndterGamleKravgrunnlagBatch.kt
+++ src/main/kotlin/no/nav/familie/tilbake/kravgrunnlag/batch/HåndterGamleKravgrunnlagBatch.kt
@@ -12,6 +12,7 @@ import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.internal.TaskService
 import no.nav.familie.tilbake.behandling.FagsystemUtil
 import no.nav.familie.tilbake.config.PropertyName
+import no.nav.familie.tilbake.kravgrunnlag.domain.ØkonomiXmlMottatt
 import no.nav.familie.tilbake.kravgrunnlag.ØkonomiXmlMottattService
 import org.slf4j.LoggerFactory
 import org.springframework.core.env.Environment
@@ -20,6 +21,8 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.Properties
 
 @Service
@@ -34,18 +37,22 @@ class HåndterGamleKravgrunnlagBatch(
     @Transactional
     fun utfør() {
         val erLeader = LeaderClient.isLeader() == true
+
         val erLokaltMiljø =
             environment.activeProfiles.any {
                 it.contains("local") || it.contains("integrasjonstest")
             }
+
         if (!erLeader && !erLokaltMiljø) {
             return
         }
 
         logger.info("Starter HåndterGamleKravgrunnlagBatch..")
+
         logger.info("Henter kravgrunnlag som er eldre enn $ALDERSGRENSE_I_UKER uker")
-        val mottattXmlIdsMedYtelse =
-            mottattXmlService.hentFrakobletGamleMottattXmlIds(
+
+        val frakobletKravgrunnlag =
+            mottattXmlService.hentFrakobletKravgrunnlag(
                 beregnBestemtDato(BARNETRYGD),
                 beregnBestemtDato(BARNETILSYN),
                 beregnBestemtDato(OVERGANGSSTØNAD),
@@ -53,53 +60,39 @@ class HåndterGamleKravgrunnlagBatch(
                 beregnBestemtDato(KONTANTSTØTTE),
             )
 
-        if (mottattXmlIdsMedYtelse.isNotEmpty()) {
-            logger.info(
-                "Det finnes ${mottattXmlIdsMedYtelse.size} kravgrunnlag som er eldre enn " +
-                    "$ALDERSGRENSE_I_UKER uker fra dagens dato",
-            )
+        val frakobletKravgrunnlagGruppertPåEksternFagsakId = frakobletKravgrunnlag.groupBy { it.eksternFagsakId }
 
-            val alleFeiledeTasker =
-                taskService.finnTasksMedStatus(
-                    listOf(
-                        Status.FEILET,
-                        Status.KLAR_TIL_PLUKK,
-                        Status.MANUELL_OPPFØLGING,
-                    ),
-                    Pageable.unpaged(),
-                )
-            mottattXmlIdsMedYtelse.forEach { mottattXmlIdOgYtelse ->
-                val finnesTask =
-                    alleFeiledeTasker.any {
-                        it.payload == mottattXmlIdOgYtelse.id.toString() &&
-                            (it.type == GammelKravgrunnlagTask.TYPE || it.type == HentFagsystemsbehandlingTask.TYPE)
-                    }
-                if (!finnesTask) {
-                    val fagsystem = FagsystemUtil.hentFagsystemFraYtelsestype(mottattXmlIdOgYtelse.ytelsestype)
-                    taskService.save(
-                        Task(
-                            type = HentFagsystemsbehandlingTask.TYPE,
-                            payload = mottattXmlIdOgYtelse.id.toString(),
-                            properties =
-                                Properties().apply {
-                                    setProperty(
-                                        PropertyName.FAGSYSTEM,
-                                        fagsystem.name,
-                                    )
-                                },
-                        ),
-                    )
+        if (frakobletKravgrunnlagGruppertPåEksternFagsakId.isEmpty()) {
+            logger.info("Det finnes ingen kravgrunnlag som er eldre enn $ALDERSGRENSE_I_UKER uker fra dagens dato")
+            logger.info("Stopper HåndterGamleKravgrunnlagBatch..")
+            return
+        }
+
+        logger.info(
+            "Det finnes ${frakobletKravgrunnlag.size} kravgrunnlag som er eldre enn " +
+                    "$ALDERSGRENSE_I_UKER uker fra dagens dato",
+        )
+
+        frakobletKravgrunnlagGruppertPåEksternFagsakId.forEach { (_, kravgrunnlagerPåFagsak) ->
+
+            val kravgrunnlagSortertEtterKontrollfelt = sorterKravgrunnlagPåKontrollfelt(kravgrunnlagerPåFagsak)
+
+            kravgrunnlagSortertEtterKontrollfelt.forEachIndexed { index, kravgrunnlagPåFagsak ->
+
+                val finnesAlleredeTaskPåKravgrunnlag = finnesAlleredeTaskForKravgrunnlag(kravgrunnlagPåFagsak)
+
+                if (!finnesAlleredeTaskPåKravgrunnlag) {
+                    taskService.save(opprettSpredtTaskForKravgrunnlagBasertPåIndex(index, kravgrunnlagPåFagsak))
                 } else {
                     logger.info(
                         "Det finnes allerede en feilet HåndterGammelKravgrunnlagTask " +
-                            "eller HentFagsystemsbehandlingTask " +
-                            "på det samme kravgrunnlaget med id ${mottattXmlIdOgYtelse.id}",
+                                "eller HentFagsystemsbehandlingTask " +
+                                "på det samme kravgrunnlaget med id ${kravgrunnlagPåFagsak.id}",
                     )
                 }
             }
-        } else {
-            logger.info("Det finnes ingen kravgrunnlag som er eldre enn $ALDERSGRENSE_I_UKER uker fra dagens dato")
         }
+
         logger.info("Stopper HåndterGamleKravgrunnlagBatch..")
     }
 
@@ -107,7 +100,50 @@ class HåndterGamleKravgrunnlagBatch(
         return LocalDate.now().minusWeeks(ALDERSGRENSE_I_UKER.getValue(ytelsestype))
     }
 
+    private fun sorterKravgrunnlagPåKontrollfelt(kravgrunnlagerPåFagsak: List<ØkonomiXmlMottatt>) =
+        kravgrunnlagerPåFagsak.sortedBy {
+            val kontrollfelt = it.kontrollfelt ?: throw IllegalStateException("Kontrollfelt eksisterer ikke på kravgrunnlag med id ${it.id}")
+            LocalDate.parse(kontrollfelt, KONTROLLFELT_DATE_TIME_FORMATTER)
+        }
+
+    private fun finnesAlleredeTaskForKravgrunnlag(kravgrunnlag: ØkonomiXmlMottatt): Boolean {
+        val taskerMedStatus = taskService.finnTasksMedStatus(
+            listOf(
+                Status.FEILET,
+                Status.KLAR_TIL_PLUKK,
+                Status.MANUELL_OPPFØLGING,
+            ),
+            Pageable.unpaged(),
+        )
+        return taskerMedStatus.any {
+            val harRiktigType = it.type == GammelKravgrunnlagTask.TYPE || it.type == HentFagsystemsbehandlingTask.TYPE
+            it.payload == kravgrunnlag.id.toString() && harRiktigType
+        }
+    }
+
+    private fun opprettSpredtTaskForKravgrunnlagBasertPåIndex(
+        index: Int,
+        kravgrunnlagPåFagsak: ØkonomiXmlMottatt,
+    ): Task {
+        // Dersom det eksistere flere kravgunnlag på samme eksternFagsakId ønsker vi å spre utførelsen
+        // av taskene for å unngå at flere behandlinger blir opprettet på samme fagsak
+        val triggerTid = LocalDateTime.now().plusHours(index.toLong())
+        return Task(
+            type = HentFagsystemsbehandlingTask.TYPE,
+            payload = kravgrunnlagPåFagsak.id.toString(),
+            properties =
+            Properties().apply {
+                setProperty(
+                    PropertyName.FAGSYSTEM,
+                    FagsystemUtil.hentFagsystemFraYtelsestype(kravgrunnlagPåFagsak.ytelsestype).name,
+                )
+            },
+        ).medTriggerTid(triggerTid)
+    }
+
     companion object {
+        val KONTROLLFELT_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm.ss.SSSSSS")
+
         val ALDERSGRENSE_I_UKER =
             mapOf<Ytelsestype, Long>(
                 BARNETRYGD to 8,
