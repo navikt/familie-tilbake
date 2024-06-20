fafa7f50305cfeda921de48abca3da533599cb28
Mattis Janitz <mattis.janitz@nav.no>
2024-06-20 08:02:07 +0200

    Springboot 326 (#1473)

--- src/main/kotlin/no/nav/familie/tilbake/vilkårsvurdering/VilkårsvurderingRepository.kt
+++ src/main/kotlin/no/nav/familie/tilbake/vilkårsvurdering/VilkårsvurderingRepository.kt
@@ -3,6 +3,7 @@ package no.nav.familie.tilbake.vilkårsvurdering
 import no.nav.familie.tilbake.common.repository.InsertUpdateRepository
 import no.nav.familie.tilbake.common.repository.RepositoryInterface
 import no.nav.familie.tilbake.vilkårsvurdering.domain.Vilkårsvurdering
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
