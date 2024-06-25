f8064a97797710da16ce62f0037c7e7d6458aa5d
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2024-06-25 09:37:16 +0200

    Bruk saksbehandlingstype, ikke tilbakekrevingsvalg (#1481)
    Co-authored-by: ma10s <mattis.janitz@nav.no>

--- src/main/kotlin/no/nav/familie/tilbake/behandling/batch/AutomatiskSaksbehandlingTask.kt
+++ src/main/kotlin/no/nav/familie/tilbake/behandling/batch/AutomatiskSaksbehandlingTask.kt
@@ -1,11 +1,12 @@
 package no.nav.familie.tilbake.behandling.batch
 
 import no.nav.familie.kontrakter.felles.Fagsystem
-import no.nav.familie.kontrakter.felles.tilbakekreving.Tilbakekrevingsvalg
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.tilbake.behandling.BehandlingRepository
+import no.nav.familie.tilbake.behandling.domain.Behandling
+import no.nav.familie.tilbake.behandling.domain.Saksbehandlingstype
 import no.nav.familie.tilbake.common.exceptionhandler.Feil
 import no.nav.familie.tilbake.common.repository.findByIdOrThrow
 import no.nav.familie.tilbake.config.Constants
@@ -37,23 +38,30 @@ class AutomatiskSaksbehandlingTask(
         logger.info("AutomatiskSaksbehandlingTask prosesserer med id=${task.id} og metadata ${task.metadata}")
         val behandlingId = UUID.fromString(task.payload)
         val behandling = behandlingRepository.findByIdOrThrow(behandlingId)
+        validerOmAutomatiskBehandlingUnder4RettsgebyrErMulig(behandlingId, behandling)
+        automatiskSaksbehandlingService.settSaksbehandlingstypeTilAutomatiskHvisOrdinær(behandlingId)
+
+        automatiskSaksbehandlingService.behandleAutomatisk(behandlingId)
+    }
+
+    private fun validerOmAutomatiskBehandlingUnder4RettsgebyrErMulig(
+        behandlingId: UUID,
+        behandling: Behandling,
+    ) {
         if (kravgrunnlagService.sumFeilutbetalingsbeløpForBehandlingId(behandlingId) > Constants.FIRE_X_RETTSGEBYR &&
-            behandling.aktivFagsystemsbehandling.tilbakekrevingsvalg == Tilbakekrevingsvalg.OPPRETT_TILBAKEKREVING_AUTOMATISK
+            behandling.saksbehandlingstype == Saksbehandlingstype.AUTOMATISK_IKKE_INNKREVING_UNDER_4X_RETTSGEBYR
         ) {
             throw Feil("Skal ikke behandle beløp over 4x rettsgebyr automatisk")
         }
 
         if (!featureToggleService.isEnabled(FeatureToggleConfig.AUTOMATISK_BEHANDLE_TILBAKEKREVING_UNDER_4X_RETTSGEBYR) &&
-            behandling.aktivFagsystemsbehandling.tilbakekrevingsvalg == Tilbakekrevingsvalg.OPPRETT_TILBAKEKREVING_AUTOMATISK
+            behandling.saksbehandlingstype == Saksbehandlingstype.AUTOMATISK_IKKE_INNKREVING_UNDER_4X_RETTSGEBYR
         ) {
             throw Feil(
                 "Behandler ikke feilutbetalinger under 4 rettsgebyr automatisk da featuretoggle for dette er skrudd av " +
                     "(${FeatureToggleConfig.AUTOMATISK_BEHANDLE_TILBAKEKREVING_UNDER_4X_RETTSGEBYR})",
             )
         }
-        automatiskSaksbehandlingService.oppdaterBehandling(behandlingId)
-
-        automatiskSaksbehandlingService.behandleAutomatisk(behandlingId)
     }
 
     companion object {
