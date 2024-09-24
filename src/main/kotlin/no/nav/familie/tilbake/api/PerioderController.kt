e5985240ab93af0503abd897d0d8408c1bc7650d
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2024-09-24 15:25:42 +0200

    Endrer slåSammenPerioder til post mapping

--- src/main/kotlin/no/nav/familie/tilbake/api/PerioderController.kt
+++ src/main/kotlin/no/nav/familie/tilbake/api/PerioderController.kt
@@ -14,6 +14,7 @@ import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -49,7 +50,7 @@ class PerioderController(
         )
 
     @Operation(summary = "Skal oppdatere skalSammenslåPerioder")
-    @GetMapping(
+    @PostMapping(
         "/slaa-sammen-perioder/{behandlingId}",
         produces = [MediaType.APPLICATION_JSON_VALUE],
     )
