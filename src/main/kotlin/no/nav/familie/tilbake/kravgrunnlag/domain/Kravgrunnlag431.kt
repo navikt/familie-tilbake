2f599de5da8e8e095b5390dec023be3a67c1f54a
bragejahren <70642183+bragejahren@users.noreply.github.com>
2024-12-16 10:08:54 +0100

    Sørger for at vi bruker korrekt klassekode for utvidet ved iverksettelse (#1589)

--- src/main/kotlin/no/nav/familie/tilbake/kravgrunnlag/domain/Kravgrunnlag431.kt
+++ src/main/kotlin/no/nav/familie/tilbake/kravgrunnlag/domain/Kravgrunnlag431.kt
@@ -163,6 +163,8 @@ enum class Klassekode(
     TREK_KODER(""), // Felles klassekode for alle TREK klassetyper
     ;
 
+    fun tilKlassekodeNavn(): String = this.overstyrtKlassekode.ifEmpty { this.name }
+
     companion object {
         fun fraKode(
             kode: String,
