5b15e202235d67cbdd0b771eef38e71d3dbf915a
Fredrik Garseg Mørk <31205303+fredrikmork@users.noreply.github.com>
2025-02-18 15:24:32 +0100

    Endret namespace for hentMiljøVariabler-lokal (#1636)

--- src/test/resources/hentMiljøvariabler-lokal.sh
+++ src/test/resources/hentMiljøvariabler-lokal.sh
@@ -1,5 +1,5 @@
 kubectl config use-context dev-gcp
-TILBAKE_LOKAL_SECRETS=$(kubectl -n teamfamilie get secret azuread-familie-tilbake-lokal -o json | jq '.data | map_values(@base64d)');
+TILBAKE_LOKAL_SECRETS=$(kubectl -n tilbake get secret azuread-tilbakekreving-lokal -o json | jq '.data | map_values(@base64d)');
 
 AZURE_APP_CLIENT_ID=$(echo "$TILBAKE_LOKAL_SECRETS" | jq -r '.AZURE_APP_CLIENT_ID')
 AZURE_APP_CLIENT_SECRET=$(echo "$TILBAKE_LOKAL_SECRETS" | jq -r '.AZURE_APP_CLIENT_SECRET')
