8ffce333ffa4fe8a542c8c94a03175df5e00453b
Kevin Sillerud <kevin.sillerud@gmail.com>
2025-02-26 13:02:54 +0100

    Legg til manglende typer verge

--- src/main/kotlin/no/nav/familie/tilbake/kontrakter/tilbakekreving/Verge.kt
+++ src/main/kotlin/no/nav/familie/tilbake/kontrakter/tilbakekreving/Verge.kt
@@ -15,5 +15,9 @@ enum class Vergetype(
     val navn: String,
 ) {
     VERGE_FOR_BARN("Verge for barn under 18 år"),
+    VERGE_FOR_FORELDRELØST_BARN("Verge for foreldreløst barn under 18 år"),
+    VERGE_FOR_VOKSEN("Verge for voksen"),
     ADVOKAT("Advokat/advokatfullmektig"),
+    ANNEN_FULLMEKTIG("Annen fullmektig"),
+    UDEFINERT("Udefinert"),
 }
