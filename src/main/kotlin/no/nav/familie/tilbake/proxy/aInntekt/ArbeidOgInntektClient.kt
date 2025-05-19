3da231217587e5daf0cdb084adc9426958a11441
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-05-19 15:52:38 +0200

    Token for ainntekt client
    Co-author-by Kevin Sillerud <kevin.sillerud@gmail.com>

--- src/main/kotlin/no/nav/familie/tilbake/proxy/aInntekt/ArbeidOgInntektClient.kt
+++ src/main/kotlin/no/nav/familie/tilbake/proxy/aInntekt/ArbeidOgInntektClient.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.tilbake.proxy.aInntekt
 
+import com.nimbusds.oauth2.sdk.GrantType
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
@@ -8,6 +9,8 @@ import io.ktor.client.request.header
 import io.ktor.http.URLBuilder
 import io.ktor.http.appendPathSegments
 import io.ktor.http.takeFrom
+import io.ktor.http.toURI
+import no.nav.familie.tilbake.http.BearerTokenClientCredentialsClientInterceptor
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
@@ -15,20 +18,25 @@ import org.springframework.stereotype.Service
 class ArbeidOgInntektClient(
     @Value("ainntekt.baseUrl")
     private val baseUrl: String,
+    private val tokenClient: BearerTokenClientCredentialsClientInterceptor,
 ) {
     private val httpClient = HttpClient(CIO)
     private val redirectUri = URLBuilder()
         .apply {
             takeFrom(baseUrl)
             appendPathSegments("api", "v2", "redirect", "sok", "a-inntekt")
-        }.buildString()
+        }.build()
 
     suspend fun hentAInntektUrl(
         personIdent: String,
-    ): String =
-        httpClient
+    ): String {
+        val clientProperties = tokenClient.clientPropertiesForGrantType(tokenClient.findByURI(redirectUri.toURI()), GrantType.CLIENT_CREDENTIALS, redirectUri.toURI())
+        val token = tokenClient.genererAccessToken(clientProperties)
+        return httpClient
             .get(redirectUri) {
                 header("Nav-Personident", personIdent)
                 header("Accept", "text/plain")
+                header("Authorization", "Bearer $token")
             }.body()
+    }
 }
