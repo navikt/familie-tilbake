9bb0288de2b3c18b18988b08d82b00d5b4b763fc
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-08-28 12:34:48 +0200

    Tok tilbake brukt endepunkt men fjernet funksjonalitet for ny modell

--- src/main/kotlin/no/nav/familie/tilbake/api/BeregningController.kt
+++ src/main/kotlin/no/nav/familie/tilbake/api/BeregningController.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.tilbake.api
 
 import io.swagger.v3.oas.annotations.Operation
+import jakarta.validation.Valid
 import no.nav.familie.tilbake.beregning.TilbakekrevingsberegningService
 import no.nav.familie.tilbake.kontrakter.Ressurs
 import no.nav.familie.tilbake.sikkerhet.AuditLoggerEvent
@@ -8,11 +9,15 @@ import no.nav.familie.tilbake.sikkerhet.Behandlerrolle
 import no.nav.familie.tilbake.sikkerhet.TilgangskontrollService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.tilbakekreving.TilbakekrevingService
+import no.nav.tilbakekreving.api.v1.dto.BeregnetPerioderDto
 import no.nav.tilbakekreving.api.v1.dto.BeregningsresultatDto
+import no.nav.tilbakekreving.kontrakter.periode.Datoperiode
 import org.springframework.http.MediaType
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
@@ -26,6 +31,25 @@ class BeregningController(
     private val tilgangskontrollService: TilgangskontrollService,
     private val tilbakekrevingService: TilbakekrevingService,
 ) {
+    @Operation(summary = "Beregn feilutbetalt beløp for nye delte perioder")
+    @PostMapping(
+        path = ["{behandlingId}/beregn/v1"],
+        produces = [MediaType.APPLICATION_JSON_VALUE],
+    )
+    fun beregnBeløp(
+        @PathVariable("behandlingId") behandlingId: UUID,
+        @Valid @RequestBody
+        perioder: List<Datoperiode>,
+    ): Ressurs<BeregnetPerioderDto> {
+        tilgangskontrollService.validerTilgangBehandlingID(
+            behandlingId = behandlingId,
+            minimumBehandlerrolle = Behandlerrolle.SAKSBEHANDLER,
+            auditLoggerEvent = AuditLoggerEvent.ACCESS,
+            handling = "Beregner feilutbetalt beløp for nye delte perioder",
+        )
+        return Ressurs.success(tilbakekrevingsberegningService.beregnBeløp(behandlingId, perioder))
+    }
+
     @Operation(summary = "Hent beregningsresultat")
     @GetMapping(
         path = ["{behandlingId}/beregn/resultat/v1"],
