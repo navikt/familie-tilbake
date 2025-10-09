8df18ee3776003443293d143971de5ac82fb10dd
amiraliw <amir.ali.wafai@gmail.com>
2025-10-09 14:31:43 +0200

    håndter i SendVarselbrev

--- modell/src/main/kotlin/no/nav/tilbakekreving/tilstand/SendVarselbrev.kt
+++ modell/src/main/kotlin/no/nav/tilbakekreving/tilstand/SendVarselbrev.kt
@@ -3,6 +3,7 @@ package no.nav.tilbakekreving.tilstand
 import no.nav.tilbakekreving.Tilbakekreving
 import no.nav.tilbakekreving.behov.VarselbrevBehov
 import no.nav.tilbakekreving.brev.Varselbrev
+import no.nav.tilbakekreving.hendelse.FagsysteminfoHendelse
 import no.nav.tilbakekreving.hendelse.VarselbrevSendtHendelse
 import no.nav.tilbakekreving.kontrakter.tilstand.TilbakekrevingTilstand
 
@@ -55,5 +56,9 @@ object SendVarselbrev : Tilstand {
         }
 
         tilbakekreving.byttTilstand(TilBehandling)
+    }
+
+    override fun håndter(tilbakekreving: Tilbakekreving, fagsysteminfo: FagsysteminfoHendelse) {
+        tilbakekreving.oppdaterFagsysteminfo(fagsysteminfo)
     }
 }
