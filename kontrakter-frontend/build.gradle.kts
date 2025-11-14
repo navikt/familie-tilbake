d9e7c37e98874d18c3bcf7189e788764ee309032
Kevin Sillerud <kevin.sillerud@gmail.com>
2025-11-14 12:58:20 +0100

    Generer frontend API interfaces fra OpenAPI kontrakter

---
+++ kontrakter-frontend/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jlleitschuh.gradle.ktlint.tasks.KtLintCheckTask
+
+plugins {
+    kotlin("jvm")
+    id("org.openapi.generator") version "7.17.0"
+}
+
+group = "no.nav"
+
+openApiGenerate {
+    generatorName = "kotlin-spring"
+    remoteInputSpec = "https://raw.githubusercontent.com/navikt/tilbakekreving-kontrakter/refs/heads/main/tsp-output/schema/openapi.yaml"
+    outputDir = "$projectDir/src/main/generated"
+    packageName = "no.nav.kontrakter.frontend"
+    cleanupOutput = true
+    configOptions.put("dateLibrary", "java8")
+    configOptions.put("interfaceOnly", "true")
+    configOptions.put("useSpringBoot3", "true")
+    configOptions.put("sourceFolder", "/")
+    globalProperties.put("models", "")
+    globalProperties.put("apis", "")
+}
+
+sourceSets {
+    main {
+        kotlin.srcDir("src/main/generated")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    dependsOn("openApiGenerate")
+}
+
+tasks.withType<KtLintCheckTask> {
+    dependsOn("openApiGenerate")
+}
+
+ktlint {
+    ignoreFailures = true
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api("org.springframework.boot:spring-boot-starter-web:3.5.4")
+    api("io.swagger.core.v3:swagger-annotations:2.2.40")
+    api("jakarta.validation:jakarta.validation-api:3.1.1")
+}
