2a707679b8f52b7b89015ca3920a973f25d79289
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-11-26 09:16:00 +0100

    Legger til autentisering

--- src/main/kotlin/no/nav/tilbakekreving/api/BehandlingApiController.kt
+++ src/main/kotlin/no/nav/tilbakekreving/api/BehandlingApiController.kt
@@ -1,5 +1,8 @@
 package no.nav.tilbakekreving.api
 
+import no.nav.familie.tilbake.sikkerhet.AuditLoggerEvent
+import no.nav.familie.tilbake.sikkerhet.Behandlerrolle
+import no.nav.familie.tilbake.sikkerhet.TilgangskontrollService
 import no.nav.kontrakter.frontend.apis.BehandlingApi
 import no.nav.kontrakter.frontend.models.FaktaDto
 import no.nav.tilbakekreving.TilbakekrevingService
@@ -8,10 +11,21 @@ import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
-class BehandlingApiController(private val tilbakekrevingService: TilbakekrevingService) : BehandlingApi {
+class BehandlingApiController(
+    private val tilbakekrevingService: TilbakekrevingService,
+    private val tilgangskontrollService: TilgangskontrollService,
+) : BehandlingApi {
     override fun fakta(behandlingId: String): ResponseEntity<FaktaDto> {
         val tilbakekreving = tilbakekrevingService.hentTilbakekreving(UUID.fromString(behandlingId))
             ?: return ResponseEntity.notFound().build()
+
+        tilgangskontrollService.validerTilgangTilbakekreving(
+            tilbakekreving = tilbakekreving,
+            behandlingId = UUID.fromString(behandlingId),
+            minimumBehandlerrolle = Behandlerrolle.SAKSBEHANDLER,
+            auditLoggerEvent = AuditLoggerEvent.ACCESS,
+            handling = "Henter fakta om feilutbetalingen",
+        )
         return ResponseEntity.ok(tilbakekreving.behandlingHistorikk.nåværende().entry.nyFaktastegFrontendDto())
     }
 }
