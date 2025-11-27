1ecefd86dbaf0d7bbc5dace80c0b943a71eb3c49
Magnus Tønnessen <magnustonnessen@gmail.com>
2025-11-27 08:13:12 +0100

    Opprett endepunkt for å kunne endre behandlende enhet for behandling (porteføljejustering BAKS) (#1928)
    Co-authored-by: Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>

---
+++ src/main/kotlin/no/nav/familie/tilbake/api/baks/BAKSPorteføljejusteringController.kt
@@ -0,0 +1,73 @@
+package no.nav.familie.tilbake.api.baks
+
+import jakarta.transaction.Transactional
+import no.nav.familie.tilbake.behandling.BehandlingRepository
+import no.nav.familie.tilbake.datavarehus.saksstatistikk.BehandlingTilstandService
+import no.nav.familie.tilbake.historikkinnslag.Aktør.Vedtaksløsning
+import no.nav.familie.tilbake.historikkinnslag.HistorikkService
+import no.nav.familie.tilbake.historikkinnslag.TilbakekrevingHistorikkinnslagstype
+import no.nav.familie.tilbake.integration.familie.IntegrasjonerClient
+import no.nav.familie.tilbake.kontrakter.Ressurs
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import java.util.UUID
+
+@RestController
+@RequestMapping("/api/baks/portefoljejustering")
+@ProtectedWithClaims(issuer = "azuread")
+@Validated
+class BAKSPorteføljejusteringController(
+    private val behandlingRepository: BehandlingRepository,
+    private val integrasjonerClient: IntegrasjonerClient,
+    private val historikkService: HistorikkService,
+    private val behandlingTilstandService: BehandlingTilstandService,
+) {
+    @Transactional
+    @PutMapping("/oppdater-behandlende-enhet")
+    fun oppdaterBehandlendeEnhetPåBehandling(
+        @RequestBody oppdaterBehandlendeEnhetRequest: OppdaterBehandlendeEnhetRequest,
+    ): Ressurs<String> {
+        val (behandlingEksternBrukId, nyEnhetId) = oppdaterBehandlendeEnhetRequest
+
+        val behandling = behandlingRepository.findByEksternBrukId(behandlingEksternBrukId)
+            ?: throw IllegalStateException("Fant ikke behandling for eksternBrukId=$behandlingEksternBrukId")
+
+        val gammelEnhetNavn = behandling.behandlendeEnhetsNavn
+
+        if (behandling.behandlendeEnhet == nyEnhetId) {
+            return Ressurs.success("Behandlende enhet er allerede satt til $nyEnhetId for behandling med eksternBrukId=$behandlingEksternBrukId")
+        }
+
+        val nyEnhet = integrasjonerClient.hentNavkontor(nyEnhetId)
+        behandlingRepository.update(
+            behandling.copy(
+                ansvarligSaksbehandler = Vedtaksløsning.ident,
+                ansvarligBeslutter = null,
+                behandlendeEnhet = nyEnhetId,
+                behandlendeEnhetsNavn = nyEnhet.navn,
+            ),
+        )
+
+        historikkService.lagHistorikkinnslag(
+            behandlingId = behandling.id,
+            historikkinnslagstype = TilbakekrevingHistorikkinnslagstype.ENDRET_ENHET,
+            aktør = Vedtaksløsning,
+            opprettetTidspunkt = LocalDateTime.now(),
+            beskrivelse = "Behandlende enhet endret fra $gammelEnhetNavn til ${nyEnhet.navn} i forbindelse med porteføljejustering januar 2026.",
+        )
+
+        behandlingTilstandService.opprettSendingAvBehandlingenManuelt(behandlingId = behandling.id)
+
+        return Ressurs.success("Behandlende enhet oppdatert til $nyEnhetId for behandling med eksternBrukId=$behandling")
+    }
+
+    data class OppdaterBehandlendeEnhetRequest(
+        val behandlingEksternBrukId: UUID,
+        val nyEnhet: String,
+    )
+}
---
+++ src/test/kotlin/no/nav/familie/tilbake/api/baks/BAKSPorteføljejusteringControllerTest.kt
@@ -0,0 +1,157 @@
+package no.nav.familie.tilbake.api.baks
+
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.verify
+import no.nav.familie.tilbake.api.baks.BAKSPorteføljejusteringController.OppdaterBehandlendeEnhetRequest
+import no.nav.familie.tilbake.behandling.BehandlingRepository
+import no.nav.familie.tilbake.data.Testdata
+import no.nav.familie.tilbake.datavarehus.saksstatistikk.BehandlingTilstandService
+import no.nav.familie.tilbake.historikkinnslag.Aktør.Vedtaksløsning
+import no.nav.familie.tilbake.historikkinnslag.HistorikkService
+import no.nav.familie.tilbake.historikkinnslag.TilbakekrevingHistorikkinnslagstype.ENDRET_ENHET
+import no.nav.familie.tilbake.integration.familie.IntegrasjonerClient
+import no.nav.familie.tilbake.kontrakter.Ressurs
+import no.nav.familie.tilbake.kontrakter.navkontor.NavKontorEnhet
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.util.UUID
+
+class BAKSPorteføljejusteringControllerTest {
+    private val behandlingRepository = mockk<BehandlingRepository>()
+    private val integrasjonerClient = mockk<IntegrasjonerClient>()
+    private val historikkService = mockk<HistorikkService>()
+    private val behandlingTilstandService = mockk<BehandlingTilstandService>()
+
+    private val baksPorteføljejusteringController =
+        BAKSPorteføljejusteringController(
+            behandlingRepository = behandlingRepository,
+            integrasjonerClient = integrasjonerClient,
+            historikkService = historikkService,
+            behandlingTilstandService = behandlingTilstandService,
+        )
+
+    private val nyEnhetId = "1234"
+
+    @BeforeEach
+    fun setUp() {
+        every { integrasjonerClient.hentNavkontor(nyEnhetId) } returns NavKontorEnhet(
+            enhetId = nyEnhetId.toInt(),
+            navn = "Nav Kontor",
+            enhetNr = "1",
+            status = "Eksisterer",
+        )
+    }
+
+    @Test
+    fun `skal kaste feil hvis Behandling ikke finnes for behandlingEksternBrukId`() {
+        // Arrange
+        val behandlingEksternBrukId = UUID.randomUUID()
+        val request =
+            OppdaterBehandlendeEnhetRequest(
+                behandlingEksternBrukId = behandlingEksternBrukId,
+                nyEnhet = nyEnhetId,
+            )
+
+        every { behandlingRepository.findByEksternBrukId(behandlingEksternBrukId) } returns null
+
+        // Act & Assert
+        val exception =
+            assertThrows<IllegalStateException> {
+                baksPorteføljejusteringController.oppdaterBehandlendeEnhetPåBehandling(request)
+            }
+
+        assertThat(exception.message).isEqualTo("Fant ikke behandling for eksternBrukId=$behandlingEksternBrukId")
+    }
+
+    @Test
+    fun `skal returnere melding om at enhet allerede er satt hvis behandlende enhet er lik ny enhet`() {
+        // Arrange
+        val behandlingEksternBrukId = UUID.randomUUID()
+        val behandling = Testdata
+            .lagBehandling(fagsakId = UUID.randomUUID())
+            .copy(behandlendeEnhet = nyEnhetId)
+
+        val request =
+            OppdaterBehandlendeEnhetRequest(
+                behandlingEksternBrukId = behandlingEksternBrukId,
+                nyEnhet = nyEnhetId,
+            )
+
+        every { behandlingRepository.findByEksternBrukId(behandlingEksternBrukId) } returns behandling
+
+        // Act
+        val result = baksPorteføljejusteringController.oppdaterBehandlendeEnhetPåBehandling(request)
+
+        // Assert
+        assertThat(result.status).isEqualTo(Ressurs.Status.SUKSESS)
+        assertThat(result.data).isEqualTo("Behandlende enhet er allerede satt til $nyEnhetId for behandling med eksternBrukId=$behandlingEksternBrukId")
+
+        verify(exactly = 0) { historikkService.lagHistorikkinnslag(behandling.id, any(), any(), any(), any()) }
+        verify(exactly = 0) { behandlingTilstandService.opprettSendingAvBehandlingenManuelt(behandling.id) }
+        verify(exactly = 0) {
+            behandlingRepository.update(
+                behandling.copy(
+                    ansvarligSaksbehandler = Vedtaksløsning.ident,
+                    ansvarligBeslutter = null,
+                    behandlendeEnhet = nyEnhetId,
+                    behandlendeEnhetsNavn = "Nav Kontor",
+                ),
+            )
+        }
+    }
+
+    @Test
+    fun `skal oppdatere behandlende enhet, opprette behandlingshistorikk og sende melding til statistikk`() {
+        // Arrange
+        val behandlingEksternBrukId = UUID.randomUUID()
+        val behandling = Testdata.lagBehandling(fagsakId = UUID.randomUUID())
+
+        val request =
+            OppdaterBehandlendeEnhetRequest(
+                behandlingEksternBrukId = behandlingEksternBrukId,
+                nyEnhet = nyEnhetId,
+            )
+
+        every { behandlingRepository.findByEksternBrukId(behandlingEksternBrukId) } returns behandling
+        every { behandlingRepository.update(any()) } returns mockk()
+        every { historikkService.lagHistorikkinnslag(any(), any(), any(), any(), any()) } returns mockk()
+        every { behandlingTilstandService.opprettSendingAvBehandlingenManuelt(any()) } just runs
+
+        // Act
+        val result = baksPorteføljejusteringController.oppdaterBehandlendeEnhetPåBehandling(request)
+
+        // Assert
+        assertThat(result.status).isEqualTo(Ressurs.Status.SUKSESS)
+        assertThat(result.data).isEqualTo("Behandlende enhet oppdatert til $nyEnhetId for behandling med eksternBrukId=$behandling")
+
+        verify(exactly = 1) {
+            behandlingRepository.update(
+                behandling.copy(
+                    ansvarligSaksbehandler = Vedtaksløsning.ident,
+                    ansvarligBeslutter = null,
+                    behandlendeEnhet = nyEnhetId,
+                    behandlendeEnhetsNavn = "Nav Kontor",
+                ),
+            )
+        }
+
+        verify(exactly = 1) {
+            historikkService.lagHistorikkinnslag(
+                behandlingId = behandling.id,
+                historikkinnslagstype = ENDRET_ENHET,
+                aktør = Vedtaksløsning,
+                opprettetTidspunkt = any(),
+                beskrivelse = "Behandlende enhet endret fra testverdi til Nav Kontor i forbindelse med porteføljejustering januar 2026.",
+            )
+        }
+
+        verify(exactly = 1) {
+            behandlingTilstandService.opprettSendingAvBehandlingenManuelt(behandling.id)
+        }
+    }
+}
