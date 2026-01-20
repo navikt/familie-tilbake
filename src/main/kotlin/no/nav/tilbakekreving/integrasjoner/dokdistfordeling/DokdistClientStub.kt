7390e4ee6d682804096331b844fcbf4a85eb7221
Amir Ali Wafai <amir.ali.wafai@gmail.com>
2026-01-20 10:12:21 +0100

    fikset integrasjon tester for når brevutsending toggle er true (#1959)

--- src/main/kotlin/no/nav/tilbakekreving/integrasjoner/dokdistfordeling/DokdistClientStub.kt
+++ src/main/kotlin/no/nav/tilbakekreving/integrasjoner/dokdistfordeling/DokdistClientStub.kt
@@ -1,11 +1,15 @@
 package no.nav.tilbakekreving.integrasjoner.dokdistfordeling
 
+import no.nav.familie.tilbake.http.RessursException
+import no.nav.familie.tilbake.kontrakter.Ressurs
 import no.nav.familie.tilbake.kontrakter.dokdist.Distribusjonstidspunkt
 import no.nav.familie.tilbake.kontrakter.dokdist.Distribusjonstype
 import no.nav.familie.tilbake.log.SecureLog
 import no.nav.tilbakekreving.integrasjoner.dokdistfordeling.domain.AdresseTo
 import no.nav.tilbakekreving.integrasjoner.dokdistfordeling.domain.DistribuerJournalpostResponse
 import no.nav.tilbakekreving.kontrakter.ytelse.FagsystemDTO
+import org.springframework.http.HttpStatus
+import org.springframework.web.client.RestClientResponseException
 import java.util.UUID
 
 class DokdistClientStub : DokdistClient {
@@ -18,6 +22,26 @@ class DokdistClientStub : DokdistClient {
         adresse: AdresseTo?,
         logContext: SecureLog.Context,
     ): DistribuerJournalpostResponse {
-        return DistribuerJournalpostResponse("")
+        when (journalpostId) {
+            "jpUkjentDødsbo" ->
+                throw RessursException(
+                    httpStatus = HttpStatus.GONE,
+                    ressurs = Ressurs.failure("Ukjent adresse dødsbo"),
+                    cause = RestClientResponseException("Ukjent adresse dødsbo", 410, "gone", null, null, null),
+                )
+            "jpUkjentAdresse" ->
+                throw RessursException(
+                    httpStatus = HttpStatus.BAD_REQUEST,
+                    ressurs = Ressurs.failure("Mottaker har ukjent adresse"),
+                    cause = RestClientResponseException("Mottaker har ukjent adresse", 401, "not there", null, null, null),
+                )
+            "jpDuplikatDistribusjon" ->
+                throw RessursException(
+                    httpStatus = HttpStatus.CONFLICT,
+                    ressurs = Ressurs.failure("Dokumentet er allerede distribuert"),
+                    cause = RestClientResponseException("Dokumentet er allerede distribuert", 409, "conflict", null, null, null),
+                )
+            else -> return DistribuerJournalpostResponse("42")
+        }
     }
 }
