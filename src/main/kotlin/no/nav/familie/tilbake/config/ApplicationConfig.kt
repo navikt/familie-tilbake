15a0c46aa65d55740129a8233d104c68fcbdeb81
Kevin Sillerud <kevin.sillerud@gmail.com>
2026-02-17 14:29:09 +0100

    Konfigurer jackson for å støtte properties med norske bokstaver

--- src/main/kotlin/no/nav/familie/tilbake/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/tilbake/config/ApplicationConfig.kt
@@ -1,6 +1,5 @@
 package no.nav.familie.tilbake.config
 
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import no.nav.familie.prosessering.config.ProsesseringInfoProvider
 import no.nav.familie.tilbake.log.LogTracingHttpFilter
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
@@ -9,6 +8,7 @@ import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer
 import org.springframework.boot.jetty.servlet.JettyServletWebServerFactory
 import org.springframework.boot.restclient.RestTemplateBuilder
 import org.springframework.boot.web.server.servlet.ServletWebServerFactory
@@ -18,8 +18,9 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Primary
 import org.springframework.http.converter.json.JacksonJsonHttpMessageConverter
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.web.client.RestTemplate
 import tools.jackson.databind.json.JsonMapper
+import tools.jackson.module.kotlin.KotlinFeature
+import tools.jackson.module.kotlin.kotlinModule
 import java.time.Duration
 import java.time.temporal.ChronoUnit
 
@@ -45,8 +46,17 @@ class ApplicationConfig {
         return filterRegistration
     }
 
+    @Primary
     @Bean
-    fun kotlinModule(): KotlinModule = KotlinModule.Builder().build()
+    fun customizeJackson(): JsonMapperBuilderCustomizer {
+        return JsonMapperBuilderCustomizer { builder ->
+            builder.addModule(
+                kotlinModule {
+                    enable(KotlinFeature.KotlinPropertyNameAsImplicitName)
+                },
+            )
+        }
+    }
 
     /**
      * Overskriver felles sin som bruker proxy, som ikke skal brukes på gcp.
@@ -54,11 +64,14 @@ class ApplicationConfig {
     @Bean
     @Primary
     fun restTemplateBuilder(jsonMapper: JsonMapper): RestTemplateBuilder {
-        val jackson2HttpMessageConverter = JacksonJsonHttpMessageConverter(jsonMapper)
         return RestTemplateBuilder()
+            .defaultMessageConverters()
             .connectTimeout(Duration.of(2, ChronoUnit.SECONDS))
             .readTimeout(Duration.of(30, ChronoUnit.SECONDS))
-            .additionalMessageConverters(listOf(jackson2HttpMessageConverter) + RestTemplate().messageConverters)
+            .customizers({ restTemplate ->
+                val index = restTemplate.messageConverters.indexOfFirst { it is JacksonJsonHttpMessageConverter }
+                restTemplate.messageConverters[index] = JacksonJsonHttpMessageConverter(jsonMapper)
+            })
     }
 
     @Bean
