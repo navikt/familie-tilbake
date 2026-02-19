01e0485c196082761a9f89a44edb2a0990f47f69
amiraliw <amir.ali.wafai@gmail.com>
2026-02-19 14:17:12 +0100

    legger til logging for feilsøking

--- src/main/kotlin/no/nav/familie/tilbake/faktaomfeilutbetaling/FaktaFeilutbetalingMapper.kt
+++ src/main/kotlin/no/nav/familie/tilbake/faktaomfeilutbetaling/FaktaFeilutbetalingMapper.kt
@@ -24,7 +24,7 @@ object FaktaFeilutbetalingMapper {
             LogiskPeriodeUtil.utledLogiskPeriode(KravgrunnlagUtil.finnFeilutbetalingPrPeriode(kravgrunnlag))
         val fagsystemsbehandling = behandling.aktivFagsystemsbehandling
         println("======>>>> faktaFeilutbetaling perioder: ${faktaFeilutbetaling?.perioder}")
-        println("======>>>> logiskePerioder perioder: ${faktaFeilutbetaling?.perioder}")
+        println("======>>>> logiskePerioder perioder: $logiskePerioder")
         val feilutbetaltePerioder =
             hentFeilutbetaltePerioder(
                 faktaFeilutbetaling = faktaFeilutbetaling,
