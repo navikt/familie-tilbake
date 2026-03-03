c9c3d569310023cda2a5201b097d3aa2bfdc1ac7
Kevin Sillerud <kevin.sillerud@gmail.com>
2026-03-03 08:32:54 +0100

    Ikke kast exception ved bruk av gammel enum type

--- modell/src/main/kotlin/no/nav/tilbakekreving/entities/BrevEntity.kt
+++ modell/src/main/kotlin/no/nav/tilbakekreving/entities/BrevEntity.kt
@@ -13,7 +13,7 @@ data class BrevEntity(
 ) {
     fun fraEntity(kravgrunnlagHistorikk: KravgrunnlagHistorikk): Brev {
         return when (brevtype) {
-            Brevtype.VARSELBREV -> {
+            Brevtype.VARSELBREV, Brevtype.VARSEL_BREV -> {
                 varselbrevEntity!!.fraEntity(
                     id,
                     brevtype,
@@ -22,9 +22,6 @@ data class BrevEntity(
             }
             Brevtype.VEDTAKSBREV -> {
                 vedtaksbrevEntity!!.fraEntity(id, brevtype)
-            }
-            Brevtype.VARSEL_BREV -> {
-                throw IllegalArgumentException("Deprecated enum")
             }
         }
     }
