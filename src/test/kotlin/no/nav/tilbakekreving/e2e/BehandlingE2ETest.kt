052d502c42659ccafc34c828ad63795c14ab523c
amiraliw <amir.ali.wafai@gmail.com>
2026-03-09 14:44:17 +0100

    sender ansvarligEnhet istedenfor null i behandlingsstatusOppdatert og vedtakFattet

--- src/test/kotlin/no/nav/tilbakekreving/e2e/BehandlingE2ETest.kt
+++ src/test/kotlin/no/nav/tilbakekreving/e2e/BehandlingE2ETest.kt
@@ -78,12 +78,12 @@ class BehandlingE2ETest : TilbakekrevingE2EBase() {
 
         dvhHendelser[0].ansvarligSaksbehandler shouldBe "VL"
         dvhHendelser[0].ansvarligBeslutter shouldBe null
-        dvhHendelser[0].ansvarligEnhet shouldBe "Ukjent"
+        dvhHendelser[0].ansvarligEnhet shouldBe "0425"
         dvhHendelser[0].behandlingsstatus shouldBe Behandlingsstatus.OPPRETTET
 
         dvhHendelser[1].ansvarligSaksbehandler shouldBe ansvarligSaksbehandler
         dvhHendelser[1].ansvarligBeslutter shouldBe null
-        dvhHendelser[1].ansvarligEnhet shouldBe "Ukjent"
+        dvhHendelser[1].ansvarligEnhet shouldBe "0425"
         dvhHendelser[1].behandlingsstatus shouldBe Behandlingsstatus.UTREDES
 
         utførSteg(
@@ -94,7 +94,7 @@ class BehandlingE2ETest : TilbakekrevingE2EBase() {
         dvhHendelser.size shouldBe 3
         dvhHendelser.last().ansvarligSaksbehandler shouldBe ansvarligSaksbehandler
         dvhHendelser.last().ansvarligBeslutter shouldBe null
-        dvhHendelser.last().ansvarligEnhet shouldBe "Ukjent"
+        dvhHendelser.last().ansvarligEnhet shouldBe "0425"
         dvhHendelser.last().behandlingsstatus shouldBe Behandlingsstatus.UTREDES
 
         utførSteg(
@@ -105,7 +105,7 @@ class BehandlingE2ETest : TilbakekrevingE2EBase() {
         dvhHendelser.size shouldBe 4
         dvhHendelser.last().ansvarligSaksbehandler shouldBe ansvarligSaksbehandler
         dvhHendelser.last().ansvarligBeslutter shouldBe null
-        dvhHendelser.last().ansvarligEnhet shouldBe "Ukjent"
+        dvhHendelser.last().ansvarligEnhet shouldBe "0425"
         dvhHendelser.last().behandlingsstatus shouldBe Behandlingsstatus.UTREDES
 
         utførSteg(
@@ -116,7 +116,7 @@ class BehandlingE2ETest : TilbakekrevingE2EBase() {
         dvhHendelser.size shouldBe 5
         dvhHendelser.last().ansvarligSaksbehandler shouldBe ansvarligSaksbehandler
         dvhHendelser.last().ansvarligBeslutter shouldBe null
-        dvhHendelser.last().ansvarligEnhet shouldBe "Ukjent"
+        dvhHendelser.last().ansvarligEnhet shouldBe "0425"
         dvhHendelser.last().behandlingsstatus shouldBe Behandlingsstatus.FATTER_VEDTAK
         kafkaProducer.finnVedtaksoppsummering(behandlingId).size shouldBe 0
 
@@ -128,12 +128,12 @@ class BehandlingE2ETest : TilbakekrevingE2EBase() {
         dvhHendelser.size shouldBe 7
         dvhHendelser[5].ansvarligSaksbehandler shouldBe ansvarligSaksbehandler
         dvhHendelser[5].ansvarligBeslutter shouldBe ansvarligBeslutter
-        dvhHendelser[5].ansvarligEnhet shouldBe "Ukjent"
+        dvhHendelser[5].ansvarligEnhet shouldBe "0425"
         dvhHendelser[5].behandlingsstatus shouldBe Behandlingsstatus.IVERKSETTER_VEDTAK
 
         dvhHendelser[6].ansvarligSaksbehandler shouldBe ansvarligSaksbehandler
         dvhHendelser[6].ansvarligBeslutter shouldBe ansvarligBeslutter
-        dvhHendelser[6].ansvarligEnhet shouldBe "Ukjent"
+        dvhHendelser[6].ansvarligEnhet shouldBe "0425"
         dvhHendelser[6].behandlingsstatus shouldBe Behandlingsstatus.AVSLUTTET
 
         val vedtaksoppsummeringer = kafkaProducer.finnVedtaksoppsummering(behandlingId)
