6f35e889bfbca92116754d54a52c2ccec8d97141
Kevin Sillerud <kevin.sillerud@gmail.com>
2026-05-18 20:49:50 +0200

    Fjern nullable til etter migreringen har kjørt

--- src/main/resources/db/migration/V81__fjern_påvent_tabell.sql
+++ src/main/resources/db/migration/V81__fjern_påvent_tabell.sql
@@ -1,3 +1,2 @@
 DROP TABLE tilbakekreving_påvent;
 ALTER TABLE tilbakekreving_behandling ADD COLUMN forrige_behandlingsstatus VARCHAR(255);
-ALTER TABLE tilbakekreving_behandling ALTER COLUMN forrige_behandlingsstatus SET NOT NULL;
