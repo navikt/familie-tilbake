cdd06afc82a12747631cbd25fd51f9e1cc8c23bc
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2026-06-02 13:48:39 +0200

    Oppdaterer testene etter rebase

--- modell/src/test/kotlin/no/nav/tilbakekreving/behandling/BehandlingRolleTest.kt
+++ modell/src/test/kotlin/no/nav/tilbakekreving/behandling/BehandlingRolleTest.kt
@@ -3,16 +3,21 @@ package no.nav.tilbakekreving.behandling
 import io.kotest.matchers.shouldBe
 import no.nav.tilbakekreving.ANSVARLIG_BESLUTTER
 import no.nav.tilbakekreving.ANSVARLIG_SAKSBEHANDLER
+import no.nav.tilbakekreving.LesContext
 import no.nav.tilbakekreving.ModellTestdata.forårsaketAvNav
 import no.nav.tilbakekreving.api.v1.dto.BehandlerRolle
 import no.nav.tilbakekreving.behandling
 import no.nav.tilbakekreving.behandlingslogg.Behandlingslogg
 import no.nav.tilbakekreving.behandlingslogg.LoggInnslag
+import no.nav.tilbakekreving.beslutterContext
+import no.nav.tilbakekreving.defaultFeatures
 import no.nav.tilbakekreving.faktastegVurdering
 import no.nav.tilbakekreving.fatteVedtakVurdering
 import no.nav.tilbakekreving.foreldelseVurdering
 import no.nav.tilbakekreving.kontrakter.periode.til
+import no.nav.tilbakekreving.lesContext
 import no.nav.tilbakekreving.saksbehandler.Behandler
+import no.nav.tilbakekreving.saksbehandlerContext
 import no.nav.tilbakekreving.test.forsettelig
 import no.nav.tilbakekreving.test.januar
 import no.nav.tilbakekreving.tilstand.Avsluttet
@@ -27,13 +32,9 @@ class BehandlingRolleTest {
     fun `veileder-rolle vises for bruker med veileder-tilgang`() {
         val behandling = behandling()
         val veileder = Behandler.Saksbehandler("veileder")
+        val lesContext = LesContext(veileder, defaultFeatures(), lesContext().klokke)
 
-        val dto = behandling.tilFrontendDto(
-            tilstand = TilBehandling,
-            behandler = veileder,
-            kanBeslutte = false,
-            behandlerRolle = BehandlerRolle.VEILEDER,
-        )
+        val dto = behandling.tilFrontendDto(tilstand = TilBehandling, lesContext = lesContext, kanBeslutte = false, behandlerRolle = BehandlerRolle.VEILEDER)
 
         dto.innloggetSaksbehandlerRolle shouldBe BehandlerRolle.VEILEDER
     }
@@ -41,8 +42,9 @@ class BehandlingRolleTest {
     @Test
     fun `saksbehandler-rolle vises for bruker med beslutter-tilgang utenfor fatte-vedtak-steget`() {
         val behandling = behandling()
+        val lesContext = LesContext(ANSVARLIG_BESLUTTER, defaultFeatures(), lesContext().klokke)
 
-        val dto = behandling.tilFrontendDto(tilstand = TilBehandling, behandler = ANSVARLIG_BESLUTTER, kanBeslutte = true, behandlerRolle = BehandlerRolle.BESLUTTER)
+        val dto = behandling.tilFrontendDto(tilstand = TilBehandling, lesContext, kanBeslutte = true, behandlerRolle = BehandlerRolle.BESLUTTER)
 
         dto.innloggetSaksbehandlerRolle shouldBe BehandlerRolle.SAKSBEHANDLER
     }
@@ -51,8 +53,9 @@ class BehandlingRolleTest {
     fun `beslutter-rolle vises for bruker med beslutter-tilgang på fatte-vedtak-steget`() {
         val behandling = behandlingKlarTilBeslutning()
         val annenBeslutter = Behandler.Saksbehandler("annen-beslutter")
+        val lesContext = LesContext(annenBeslutter, defaultFeatures(), lesContext().klokke)
 
-        val dto = behandling.tilFrontendDto(tilstand = TilBehandling, behandler = annenBeslutter, kanBeslutte = true, behandlerRolle = BehandlerRolle.BESLUTTER)
+        val dto = behandling.tilFrontendDto(tilstand = TilBehandling, lesContext, kanBeslutte = true, behandlerRolle = BehandlerRolle.BESLUTTER)
 
         dto.innloggetSaksbehandlerRolle shouldBe BehandlerRolle.BESLUTTER
     }
@@ -60,8 +63,9 @@ class BehandlingRolleTest {
     @Test
     fun `saksbehandler-rolle vises for beslutter som er ansvarlig saksbehandler på fatte-vedtak-steget`() {
         val behandling = behandlingKlarTilBeslutning()
+        val lesContext = LesContext(ANSVARLIG_SAKSBEHANDLER, defaultFeatures(), lesContext().klokke)
 
-        val dto = behandling.tilFrontendDto(tilstand = TilBehandling, behandler = ANSVARLIG_SAKSBEHANDLER, kanBeslutte = true, behandlerRolle = BehandlerRolle.BESLUTTER)
+        val dto = behandling.tilFrontendDto(tilstand = TilBehandling, lesContext, kanBeslutte = true, behandlerRolle = BehandlerRolle.BESLUTTER)
 
         dto.innloggetSaksbehandlerRolle shouldBe BehandlerRolle.SAKSBEHANDLER
     }
@@ -69,25 +73,25 @@ class BehandlingRolleTest {
     @Test
     fun `beslutter-rolle vises for ansvarlig beslutter på avsluttet behandling`() {
         val behandling = behandlingKlarTilBeslutning()
-        behandling.håndter(ANSVARLIG_BESLUTTER, fatteVedtakVurdering(), behandlingslogg)
+        val lesContext = LesContext(ANSVARLIG_BESLUTTER, defaultFeatures(), lesContext().klokke)
+        behandling.håndter(beslutterContext(), fatteVedtakVurdering())
 
-        val dto = behandling.tilFrontendDto(tilstand = Avsluttet, behandler = ANSVARLIG_BESLUTTER, kanBeslutte = true, behandlerRolle = BehandlerRolle.BESLUTTER)
+        val dto = behandling.tilFrontendDto(tilstand = Avsluttet, lesContext, kanBeslutte = true, behandlerRolle = BehandlerRolle.BESLUTTER)
 
         dto.innloggetSaksbehandlerRolle shouldBe BehandlerRolle.BESLUTTER
     }
 
     private fun behandlingKlarTilBeslutning(): Behandling {
         val behandling = behandling()
-        behandling.håndter(ANSVARLIG_SAKSBEHANDLER, faktastegVurdering(), behandlingslogg)
+        behandling.håndter(saksbehandlerContext(), faktastegVurdering())
         behandling.lagreForhåndsvarselUnntak(
             BegrunnelseForUnntak.ÅPENBART_UNØDVENDIG,
             beskrivelse = "Trenger ikke forhåndsvarsel i test",
-            behandler = ANSVARLIG_SAKSBEHANDLER,
-            behandlingslogg = behandlingslogg,
+            saksbehandlerContext(),
         )
-        behandling.håndter(ANSVARLIG_SAKSBEHANDLER, periode, foreldelseVurdering(), behandlingslogg)
-        behandling.håndter(ANSVARLIG_SAKSBEHANDLER, periode, forårsaketAvNav().burdeForstått(aktsomhet = forsettelig()), behandlingslogg)
-        behandling.håndterForeslåVedtak(ANSVARLIG_SAKSBEHANDLER, behandlingslogg)
+        behandling.håndter(saksbehandlerContext(), periode, foreldelseVurdering())
+        behandling.håndter(saksbehandlerContext(), periode, forårsaketAvNav().burdeForstått(aktsomhet = forsettelig()))
+        behandling.håndterForeslåVedtak(saksbehandlerContext())
         return behandling
     }
 }
