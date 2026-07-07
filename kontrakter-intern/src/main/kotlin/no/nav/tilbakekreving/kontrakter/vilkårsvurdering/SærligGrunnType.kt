748e8e26d5ee77feed046670e0ffffd751c73d1c
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2026-07-07 09:29:26 +0200

    Endrer særlige grunner til ny tekst

--- kontrakter-intern/src/main/kotlin/no/nav/tilbakekreving/kontrakter/vilkårsvurdering/SærligGrunnType.kt
+++ kontrakter-intern/src/main/kotlin/no/nav/tilbakekreving/kontrakter/vilkårsvurdering/SærligGrunnType.kt
@@ -3,9 +3,9 @@ package no.nav.tilbakekreving.kontrakter.vilkårsvurdering
 enum class SærligGrunnType(
     val navn: String,
 ) {
-    GRAD_AV_UAKTSOMHET("Graden av uaktsomhet hos den kravet retter seg mot"),
-    HELT_ELLER_DELVIS_NAVS_FEIL("Om feilen helt eller delvis kan tilskrives NAV"),
-    STØRRELSE_BELØP("Størrelsen på feilutbetalt beløp"),
-    TID_FRA_UTBETALING("Hvor lang tid siden utbetalingen fant sted"),
+    GRAD_AV_UAKTSOMHET("Graden av uaktsomhet hos den som kravet retter seg mot"),
+    STØRRELSE_BELØP("Størrelsen på det feilutbetalte beløpet"),
+    TID_FRA_UTBETALING("Hvor lang tid det har gått siden utbetalingen fant sted"),
+    HELT_ELLER_DELVIS_NAVS_FEIL("Om feilen helt eller delvis kan tilskrives Nav"),
     ANNET("Annet"),
 }
