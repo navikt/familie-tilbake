1e619f16154ca1cae58b94d1335f0ea77518f321
amiraliw <amir.ali.wafai@gmail.com>
2026-07-14 13:18:07 +0200

    legger til ikke_aktuelt som en unnlattelse

--- kontrakter-frontend/build.gradle.kts
+++ kontrakter-frontend/build.gradle.kts
@@ -24,7 +24,7 @@ subprojects {
     }
 
     tasks.withType<GenerateTask> {
-        remoteInputSpec = "https://raw.githubusercontent.com/navikt/tilbakekreving-kontrakter/7ab0de1f13588c47696e62bb332a78469d1cb3ff/tsp-output/schema/openapi.yaml"
+        remoteInputSpec = "https://raw.githubusercontent.com/navikt/tilbakekreving-kontrakter/2044df925bb94850f095058b6bf193c4cfeed5f8/tsp-output/schema/openapi.yaml"
         generatorName = "kotlin-spring"
         outputDir = projectDir.resolve("src/main/generated")
         packageName = "no.nav.tilbakekreving.kontrakter.frontend"
