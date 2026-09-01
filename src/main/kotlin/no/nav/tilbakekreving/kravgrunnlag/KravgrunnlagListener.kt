fe5a2b52a361dbd948796fa95016e065b61c8ed8
Kevin Sillerud <kevin.sillerud@gmail.com>
2026-09-01 09:31:38 +0200

    Logg konsumering av statusmeldinger

--- src/main/kotlin/no/nav/tilbakekreving/kravgrunnlag/KravgrunnlagListener.kt
+++ src/main/kotlin/no/nav/tilbakekreving/kravgrunnlag/KravgrunnlagListener.kt
@@ -3,8 +3,11 @@ package no.nav.tilbakekreving.kravgrunnlag
 import jakarta.jms.Message
 import jakarta.jms.MessageListener
 import jakarta.jms.TextMessage
+import net.logstash.logback.argument.StructuredArguments.keyValue
 import no.nav.familie.tilbake.config.Constants
 import no.nav.familie.tilbake.kravgrunnlag.KravgrunnlagUtil
+import no.nav.familie.tilbake.log.SecureLog
+import no.nav.familie.tilbake.log.TracedLogger
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,11 +15,20 @@ class KravgrunnlagListener(
     private val kravgrunnlagBufferRepository: KravgrunnlagBufferRepository,
     private val statusmeldingBufferRepository: StatusmeldingBufferRepository,
 ) : MessageListener {
+    private val log = TracedLogger.getLogger<KravgrunnlagListener>()
+
     override fun onMessage(message: Message) {
         require(message is TextMessage) { "Mottok melding som ikke er TextMessage" }
         val meldingXML = message.text
         if (meldingXML.contains(Constants.STATUSMELDING_XML_ROOT_ELEMENT)) {
             val statusmelding = KravgrunnlagUtil.unmarshalStatusmelding(meldingXML)
+            log.medContext(SecureLog.Context.utenBehandling(statusmelding.fagsystemId)) {
+                info(
+                    "Mottok statusmelding for {} med status {}",
+                    keyValue("vedtakId", statusmelding.vedtakId),
+                    statusmelding.kodeStatusKrav,
+                )
+            }
             statusmeldingBufferRepository.lagre(
                 StatusmeldingBufferRepository.Entity(
                     statusmelding = meldingXML,
