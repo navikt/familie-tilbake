f9b82560161b82db6fabb13a2426d78285db2bca
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2026-09-18 15:18:20 +0200

    Fjerner mellom 2006–2016 siden det gjelder fra til dato

--- felles/src/main/kotlin/no/nav/tilbakekreving/Rettsgebyr.kt
+++ felles/src/main/kotlin/no/nav/tilbakekreving/Rettsgebyr.kt
@@ -6,15 +6,6 @@ object Rettsgebyr {
     private val rettsgebyrForDato =
         listOf(
             Datobeløp(LocalDate.of(2006, 1, 1), 860),
-            Datobeløp(LocalDate.of(2007, 1, 1), 860),
-            Datobeløp(LocalDate.of(2008, 1, 1), 860),
-            Datobeløp(LocalDate.of(2009, 1, 1), 860),
-            Datobeløp(LocalDate.of(2010, 1, 1), 860),
-            Datobeløp(LocalDate.of(2011, 1, 1), 860),
-            Datobeløp(LocalDate.of(2012, 1, 1), 860),
-            Datobeløp(LocalDate.of(2013, 1, 1), 860),
-            Datobeløp(LocalDate.of(2014, 1, 1), 860),
-            Datobeløp(LocalDate.of(2015, 1, 1), 860),
             Datobeløp(LocalDate.of(2016, 1, 1), 1025),
             Datobeløp(LocalDate.of(2017, 1, 1), 1049),
             Datobeløp(LocalDate.of(2018, 1, 1), 1130),
@@ -28,15 +19,15 @@ object Rettsgebyr {
             Datobeløp(LocalDate.of(2026, 1, 1), 1345),
         )
 
-    fun rettsgebyrForÅr(år: Int) = rettsgebyrForDato.filter { it.gyldigÅr.year <= år }.maxByOrNull { it.gyldigÅr }?.beløp
+    fun rettsgebyrForÅr(år: Int) = rettsgebyrForDato.filter { it.innført.year <= år }.maxByOrNull { it.innført }?.beløp
 
-    val rettsgebyr = rettsgebyrForDato.filter { it.gyldigÅr <= LocalDate.now() }.maxByOrNull { it.gyldigÅr }!!.beløp
+    val rettsgebyr = rettsgebyrForDato.filter { it.innført <= LocalDate.now() }.maxByOrNull { it.innført }!!.beløp
 
     fun fireRettsgebyrForÅr(år: Int) = rettsgebyrForÅr(år)?.times(4)
         ?: throw IllegalStateException("Rettsgebyr for år $år er ikke definert")
 
     private class Datobeløp(
-        val gyldigÅr: LocalDate,
+        val innført: LocalDate,
         val beløp: Long,
     )
 }
