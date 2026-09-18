213844bfee5511e64010ab6f0a20fde8b8594425
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2026-09-18 15:29:12 +0200

    Kommentar på felles rettsgebyr

--- felles/src/main/kotlin/no/nav/tilbakekreving/Rettsgebyr.kt
+++ felles/src/main/kotlin/no/nav/tilbakekreving/Rettsgebyr.kt
@@ -5,6 +5,7 @@ import java.time.LocalDate
 object Rettsgebyr {
     private val rettsgebyrForDato =
         listOf(
+            // Det var samme rettsgebyr fra 2006–2015
             Datobeløp(LocalDate.of(2006, 1, 1), 860),
             Datobeløp(LocalDate.of(2016, 1, 1), 1025),
             Datobeløp(LocalDate.of(2017, 1, 1), 1049),
